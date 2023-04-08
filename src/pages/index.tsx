fa66a675b87f615b28633ea5e0f54b7cc57771f5
frostygum <4nthonius.juan5@gmail.com>
2023-04-09 01:28:36 +0700

    fix/add-styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  color: red;
+`;
 
 export default function Home(): React.ReactElement {
-  return <>happy coding tes</>;
+  return <Title>happy coding tes</Title>;
 }

aa1c13607903bc76de0ed296252e1e221cd7eba6
brian capah <briansabbath@klique.id>
2023-04-11 14:26:21 +0700

    slicing section 2

---
+++ src/containers/landing/Section2Card.tsx
@@ -0,0 +1,16 @@
+export default function Section2Card(): React.ReactElement {
+  return (
+    <div className="border rounded-2xl border-gray-300 px-2 py-10 flex items-center bg-white ">
+      <div>
+        <div>Trends</div>
+        <div>Apple : New fitur from Iphone 13 pro</div>
+        <div>
+          <span>gambar</span>
+          <span>nama</span>
+          <span>jam</span>
+        </div>
+      </div>
+      <div>gambar</div>
+    </div>
+  );
+}

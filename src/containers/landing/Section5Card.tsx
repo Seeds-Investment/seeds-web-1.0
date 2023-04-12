a7d46fc2d128657d9c190f2233678883acc4099e
brian capah <briansabbath@klique.id>
2023-04-12 15:10:33 +0700

    add icon section 5

--- src/containers/landing/Section5Card.tsx
+++ src/containers/landing/Section5Card.tsx
@@ -2,6 +2,9 @@ import { formatCurrency } from '@/utils/common/currency';
 import { ICompetitionItem } from '@/utils/interfaces/components.interfaces';
 import { Button } from '@material-tailwind/react';
 import moment from 'moment';
+import users from '@/assets/landing-page/users.png';
+import time from '@/assets/landing-page/time.png';
+import currency from '@/assets/landing-page/currency.png';
 import Image from 'next/image';
 
 export default function Section5Card({
@@ -18,15 +21,36 @@ export default function Section5Card({
         </div>
       </div>
       <div className="text-lg font-semibold mt-2">{data.title}</div>
-      <div className="tracking-widest my-2 font-thin text-sm">{`${data.participant.total}/${data.participant.total} Participants`}</div>
-      <div className="tracking-widest my-2 font-thin text-sm">
-        {moment(data.deadline).format('DD MMMM YYYY')}
-      </div>
-      <div className="tracking-widest my-2 font-thin text-sm capitalize">
-        {data.status}
-      </div>
 
-      <Button className="w-full bg-seeds-button-green mt-2 capitalize">
+      <table>
+        <tr>
+          <td className="w-[40px]">
+            <Image src={users} alt="user" className="w-[20px] h-[20px] mr-2" />
+          </td>
+          <td className=" tracking-widest my-2 font-thin text-sm flex items-center">
+            {`${data.participant.total}/${data.participant.total} Participants`}
+          </td>
+        </tr>
+
+        <tr>
+          <td className=" w-[40px]">
+            <Image src={time} alt="user" className="w-[20px] h-[20px] mr-2" />
+          </td>
+          <td className="tracking-widest my-2 font-thin text-sm flex items-center">
+            {moment(data.deadline).format('DD MMMM YYYY')}
+          </td>
+        </tr>
+
+        <tr>
+          <td className="w-[40px] flex items-center ml-1">
+            <Image src={currency} alt="user" className="w-[12px] h-[20px]" />
+          </td>
+          <td className="tracking-widest my-2 font-thin text-sm capitalize">
+            {data.status}
+          </td>
+        </tr>
+      </table>
+      <Button className="w-full bg-seeds-button-green mt-3 capitalize">
         Get A Ticket
       </Button>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,33 @@ import Section1 from '@/containers/landing/Section1';
 import Section2 from '@/containers/landing/Section2';
 import Section3 from '@/containers/landing/Section3';
 import Section5 from '@/containers/landing/Section5';
+import aa from '@/assets/landing-page/5340.png';
+import { useRef } from 'react';
+import Slider from '@ant-design/react-slick';
+import Image from 'next/image';
 
 export default function Home(): React.ReactElement {
+  const sliderRef = useRef(null);
+
+  setTimeout(() => {
+    sliderRef.current?.slickPlay();
+  }, 5000);
+
+  const a = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoPlay: true,
+    customPaging: () => <div className="dc mt-6"></div>
+  };
   return (
-    <div>
+    <>
       <Section1 />
       <Section2 />
       <Section3 />
       <Section5 />
-    </div>
+    </>
   );
 }

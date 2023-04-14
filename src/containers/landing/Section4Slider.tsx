21ec428a526e77afed910bc7bf2c04208b5d8a46
brian capah <briansabbath@klique.id>
2023-04-14 21:33:49 +0700

    precommit test

--- src/containers/landing/Section4Slider.tsx
+++ src/containers/landing/Section4Slider.tsx
@@ -1,16 +1,18 @@
 import { competitionCardList } from '@/utils/_static/dummy';
+import type { Settings } from '@ant-design/react-slick';
 import Slider from '@ant-design/react-slick';
 import React from 'react';
 import Section4Card from './Section4Card';
 
 export default function Section4Slider(): React.ReactElement {
+  const settings: Settings = {
+    slidesToShow: 2
+  };
   return (
-    <>
-      <Slider slidesToShow={2}>
-        {competitionCardList.map((data, idx) => (
-          <Section4Card key={idx} data={data} />
-        ))}
-      </Slider>
-    </>
+    <Slider {...settings}>
+      {competitionCardList.map((data, key) => (
+        <Section4Card key={key} data={data} />
+      ))}
+    </Slider>
   );
 }

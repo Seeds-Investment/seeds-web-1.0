59e5160cedcffd011fccebc1d4416e568a28d42a
brian capah <briansabbath@klique.id>
2023-04-14 20:08:47 +0700

    setup multi language

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,36 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from '@material-tailwind/react';
+import i18n from 'i18next';
+import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
+import en from 'public/locales/en';
+import id from 'public/locales/id';
+import { initReactI18next } from 'react-i18next';
 
-export default function App({
-  Component,
-  pageProps
-}: AppProps): React.ReactElement {
+function App({ Component, pageProps }: AppProps): React.ReactElement {
+  void i18n.use(initReactI18next).init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: true,
+
+    resources: {
+      en: {
+        translation: en
+      },
+      id: {
+        translation: id
+      }
+    },
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
   return (
     <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
+
+export default appWithTranslation(App);

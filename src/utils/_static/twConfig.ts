eddeb2bdbf3d969e55ae08ef4dd550aabf1bec35
brian capah <briansabbath@klique.id>
2023-04-14 21:40:26 +0700

    change import type on twconfig

--- src/utils/_static/twConfig.ts
+++ src/utils/_static/twConfig.ts
@@ -5,10 +5,6 @@ const twConfig: Config = {
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
-
-    // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {

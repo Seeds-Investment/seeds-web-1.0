e457311c28a2069e907f41909295f0a55aeaeb87
frostygum <4nthonius.juan5@gmail.com>
2023-04-19 11:43:40 +0700

    fix: example layout eith router

--- src/hooks/useService.ts
+++ src/hooks/useService.ts
@@ -9,26 +9,30 @@ const useService = (
   data: any;
   error: string;
   loading: boolean;
-  execute: (...payload: any) => void;
+  execute: (...payload: any) => Promise<void>;
 } => {
   // states
   const [data, setData] = useState<AxiosResponse | null>(null);
   const [error, setError] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
-  const execute = (...payload: any): void => {
+  const execute = async (...payload: any): Promise<void> => {
     setLoading(true);
 
-    axiosInstance(...payload)
-      .then((res: AxiosResponse) => {
-        setData(res);
-      })
-      .catch((error: AxiosError) => {
-        setError(error.message);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    await new Promise<void>((resolve, reject) => {
+      axiosInstance(...payload)
+        .then((res: AxiosResponse) => {
+          setData(res);
+          resolve();
+        })
+        .catch((error: AxiosError) => {
+          setError(error.message);
+          reject(error.message);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    });
   };
 
   return { data, error, loading, execute };
--- src/pages/sample/post/create.tsx
+++ src/pages/sample/post/create.tsx
@@ -1,4 +1,5 @@
 import { Button, Input, Typography } from '@material-tailwind/react';
+import { useRouter } from 'next/router';
 import { useMemo, useState } from 'react';
 
 import SampleLayout from '@/components/layouts/SampleLayout';
@@ -7,6 +8,7 @@ import { createPost } from '@/repository/post';
 import { isEmptyString, isUndefindOrNull } from '@/utils/common/utils';
 
 const TodoPage = (): JSX.Element => {
+  const router = useRouter();
   const { data, error, loading, execute } = useService(createPost);
 
   const [form, setForm] = useState({
@@ -44,42 +46,61 @@ const TodoPage = (): JSX.Element => {
   };
 
   const submit = (): void => {
-    execute(form);
+    void (async () => {
+      await execute(form);
+    })();
   };
 
   return (
-    <form className="mt-8 mb-2">
-      <div className="mb-4 flex flex-col gap-6">
-        <Input
-          label="Title"
-          size="lg"
-          type="text"
-          readOnly={loading}
-          disabled={loading}
-          value={form.title}
-          onChange={e => {
-            handleChangeFormData('title', e.target.value);
-          }}
-        />
-        <Input
-          label="Subtitle"
-          size="lg"
-          type="text"
-          readOnly={loading}
-          disabled={loading}
-          value={form.subtitle}
-          onChange={e => {
-            handleChangeFormData('subtitle', e.target.value);
-          }}
-        />
-        <Button disabled={loading} onClick={submit}>
-          submit
-        </Button>
-      </div>
-      <Typography color="gray" className="mt-4 text-center font-normal">
-        <RenderedData />
-      </Typography>
-    </form>
+    <>
+      <form className="mt-8 mb-2">
+        <div className="mb-4 flex flex-col gap-6">
+          <Input
+            label="Title"
+            size="lg"
+            type="text"
+            readOnly={loading}
+            disabled={loading}
+            value={form.title}
+            onChange={e => {
+              handleChangeFormData('title', e.target.value);
+            }}
+          />
+          <Input
+            label="Subtitle"
+            size="lg"
+            type="text"
+            readOnly={loading}
+            disabled={loading}
+            value={form.subtitle}
+            onChange={e => {
+              handleChangeFormData('subtitle', e.target.value);
+            }}
+          />
+          <Button
+            disabled={loading}
+            onClick={() => {
+              submit();
+            }}
+          >
+            submit
+          </Button>
+        </div>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          <RenderedData />
+        </Typography>
+      </form>
+
+      <Button
+        fullWidth
+        color="green"
+        onClick={() => {
+          void router.push('/sample/post');
+        }}
+      >
+        back to post
+      </Button>
+    </>
   );
 };
 

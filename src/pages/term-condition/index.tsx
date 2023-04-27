fdc98bfa040112874f8b0113ed084356bf191371
hotmasilaen <hotma.silaen@onebyone.io>
2023-04-27 21:56:26 +0700

    add tnc

--- src/pages/term-condition/index.tsx
+++ src/pages/term-condition/index.tsx
@@ -2,6 +2,8 @@ import CAccordion from '@/components/CAccordion';
 import Container from '@/components/Container';
 import { useTranslation } from 'react-i18next';
 import id from '../../../public/locales/id';
+import CircleMembership from './CircleMembership';
+import PrivacyPolicy from './PrivacyPolicy';
 import TermsConditions from './TermsConditions';
 
 export default function Login(): React.ReactElement {
@@ -9,15 +11,23 @@ export default function Login(): React.ReactElement {
   return (
     <div>
       <Container>
-        <p className="font-bold text-4xl">Legal</p>
-        <div>
+        <p className="font-bold text-2xl mt-8">Legal</p>
+        <div className="mt-4">
           {Object.keys(id.termAndCondition)
             ?.filter((k, i) => i > 1)
             .map((key, idx) => (
               <CAccordion
                 key={key}
                 title={t(`termAndCondition.${key}.title`)}
-                description={<TermsConditions />}
+                description={
+                  key === 'tnc' ? (
+                    <TermsConditions />
+                  ) : key === 'privacyPolicy' ? (
+                    <PrivacyPolicy />
+                  ) : (
+                    <CircleMembership />
+                  )
+                }
               />
             ))}
         </div>

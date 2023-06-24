064a417c6007766da4e10d8293f11b43dd3257f0
Fredi Anriko <fredianriko@gmail.com>
2023-06-24 23:53:06 +0700

    fix styling faq

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,4 +1,7 @@
 import CAccordion from '@/components/CAccordion';
+import CardGradient from '@/components/ui/card/CardGradient';
+import PageGradient from '@/components/ui/page-gradient/PageGradient';
+import useWindowInnerWidth from '@/hooks/useWindowInnerWidth';
 import { Button } from '@material-tailwind/react';
 import Image from 'next/image';
 import Faq from 'public/assets/faq.png';
@@ -6,13 +9,28 @@ import { useTranslation } from 'react-i18next';
 import id from '../../../public/locales/id';
 
 export default function FAQ(): React.ReactElement {
+  const width = useWindowInnerWidth();
   const { t } = useTranslation();
 
   return (
-    <div className="bg-black min-h-screen">
-      <div className="px-5 lg:px-[100px] py-10 min-h-[calc(100vh-100px)] flex justify-between bg-opacity-80 bg-white">
-        <div className="w-full rounded-2xl flex justify-center bg-white p-5 bg-opacity-50">
-          <div className=" w-full lg:w-1/2 bg-white rounded-2xl p-10">
+    <PageGradient
+      defaultGradient
+      className="z-0 sm:relative sm:pb-20 absolute overflow-hidden flex flex-col items-center w-full bottom-0"
+    >
+      <CardGradient
+        defaultGradient
+        className={`relative overflow-hidden flex flex-col items-center py-4 w-full sm:w-[90%] sm:rounded-[18px] sm:min-h-[36rem] ${
+          width !== undefined && width < 370
+            ? 'h-[38rem]'
+            : width !== undefined && width < 400
+            ? 'h-[45rem]'
+            : width !== undefined && width < 415
+            ? 'h-[48rem]'
+            : ''
+        } bg-white`}
+      >
+        <div className="w-full bg-white rounded-2xl flex justify-center p-5  ">
+          <div className=" w-[600px] lg:w-1/2  border-neutral-50 border-0 p-10">
             <br />
             <p className="font-semibold text-lg font-poppins font-18 text-neutral-600 text-center">
               {t('faq.settings')}
@@ -62,7 +80,7 @@ export default function FAQ(): React.ReactElement {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </CardGradient>
+    </PageGradient>
   );
 }

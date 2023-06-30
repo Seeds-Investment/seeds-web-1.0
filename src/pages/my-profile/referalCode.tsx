41e8dc67d22e83b70b81a33c0a7afb19f3548752
redikawest <redikawest@gmail.com>
2023-06-30 19:04:13 +0800

    fix: responsive

--- src/pages/my-profile/referalCode.tsx
+++ src/pages/my-profile/referalCode.tsx
@@ -80,7 +80,7 @@ const ReferalCode = (): JSX.Element => {
       customGradient={customGradient}
       className="absolute overflow-hidden w-full"
     >
-      <CCard className="p-5 mb-5 h-[251px] rounded-none md:rounded-lg md:mt-2 md:mx-7 lg:mx-12 bg-[#3AC4A0]">
+      <CCard className="p-5 mb-5 h-[253px] lg:h-[300px] rounded-none md:rounded-lg md:mt-2 md:mx-7 lg:mx-12 bg-[#3AC4A0]">
         <div className="flex flex-col md:flex-row items-start justify-center h-full md:justify-start md:mx-3">
           <div className="text-start md:text-left z-[2]">
             <div className="text-white font-normal text-lg md:text-3xl">
@@ -93,7 +93,7 @@ const ReferalCode = (): JSX.Element => {
           <div className="flex justify-end">
             <div
               className={
-                'absolute w-1/2 h-1/2 right-0 md:w-1/3 md:h-1/3 md:top-3 md:ml-2 lg:w-1/4 lg:h1/4 lg:top-4 z-[1] ' +
+                'absolute w-1/2 h-1/2 right-0 md:w-1/3 md:h-1/3 md:top-3 md:ml-2 lg:w-1/4 lg:h-full lg:top-4 z-[1] ' +
                 bottomPosition
               }
             >

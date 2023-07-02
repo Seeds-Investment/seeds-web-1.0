fd6a5238c48c3bbd13f9a3175f43a611953211ab
Fredi Anriko <fredianriko@gmail.com>
2023-07-02 17:35:53 +0700

    legal content from docs

--- src/pages/faq-submenu/circle-membership/index.tsx
+++ src/pages/faq-submenu/circle-membership/index.tsx
@@ -2,7 +2,6 @@ import CAccordion from '@/components/CAccordion';
 import CardGradient from '@/components/ui/card/CardGradient';
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
 import useWindowInnerWidth from '@/hooks/useWindowInnerWidth';
-import id from 'public/locales/id';
 import { useTranslation } from 'react-i18next';
 
 const CircleMembership: React.FC = () => {
@@ -29,36 +28,182 @@ const CircleMembership: React.FC = () => {
         <div className="w-full bg-white rounded-2xl flex justify-center p-5  ">
           <div className=" w-[600px] lg:w-1/2  border-neutral-50 border-0 p-10">
             <p className="font-semibold text-lg font-poppins leading-7 text-[18px]  text-stone-900 text-center">
-              {t('faq.circleMembership.title')}
+              {t('termAndCondition.circleMembership.title')}
             </p>
             <br />
             <p className="font-bold font-14 leading-5 font-poppins">
-              {t('faq.circleMembership.lastUpdated')}
+              {t('termAndCondition.circleMembership.lastupdate')}
               <span className="text-purple-700">
-                {t('faq.circleMembership.date')}
+                {t('termAndCondition.circleMembership.updatedate')}
               </span>
             </p>
             <br />
             <p className="font-poppins font-14 leading-5 text-justify">
-              {t('faq.circleMembership.overview')}
+              {t(`termAndCondition.circleMembership.announcement`)}
             </p>
             <br />
 
             <div className="">
-              {Object.keys(id.faq.circleMembership.content.title).map(
-                (key, idx) => (
-                  <CAccordion
-                    key={key}
-                    title={t(`faq.circleMembership.content.title.${idx + 1}`)}
-                    titleColor="#3AC4A0"
-                    description={
-                      <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
-                        {t(`faq.circleMembership.content.desc.${idx + 1}`)}
-                      </div>
-                    }
-                  />
-                )
-              )}
+              {/* Services */}
+              <CAccordion
+                key={1}
+                title={t(`termAndCondition.circleMembership.content.title.1`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.1`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Circle Owners Responsibilities */}
+              <CAccordion
+                key={2}
+                title={t(`termAndCondition.circleMembership.content.title.2`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.2`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Fee and Commission */}
+              <CAccordion
+                key={3}
+                title={t(`termAndCondition.circleMembership.content.title.3`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.3`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Utilization and Protection of Personal Data */}
+              <CAccordion
+                key={4}
+                title={t(`termAndCondition.circleMembership.content.title.4`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.4`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+              {/* Intellectual Property */}
+              <CAccordion
+                key={5}
+                title={t(`termAndCondition.circleMembership.content.title.5`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.5`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Limitation Of Liability */}
+              <CAccordion
+                key={6}
+                title={t(`termAndCondition.circleMembership.content.title.6`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.6`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Immediate Termination */}
+              <CAccordion
+                key={7}
+                title={t(`termAndCondition.circleMembership.content.title.7`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.7`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Governing Law */}
+              <CAccordion
+                key={8}
+                title={t(`termAndCondition.circleMembership.content.title.8`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.8`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
+
+              {/* Dispute Settlement */}
+              <CAccordion
+                key={9}
+                title={t(`termAndCondition.circleMembership.content.title.9`)}
+                description={
+                  <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                    {t(`termAndCondition.circleMembership.content.desc.9`)
+                      .split('\n')
+                      .map((paragraph, index) => (
+                        <>
+                          <p key={index}>{paragraph}</p>
+                          <br />
+                        </>
+                      ))}
+                  </div>
+                }
+              />
             </div>
           </div>
         </div>
--- src/pages/faq-submenu/terms-condition/index.tsx
+++ src/pages/faq-submenu/terms-condition/index.tsx
@@ -2,7 +2,6 @@ import CAccordion from '@/components/CAccordion';
 import CardGradient from '@/components/ui/card/CardGradient';
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
 import useWindowInnerWidth from '@/hooks/useWindowInnerWidth';
-import id from 'public/locales/id';
 import { useTranslation } from 'react-i18next';
 
 const TersmCondition: React.FC = () => {
@@ -61,24 +60,279 @@ const TersmCondition: React.FC = () => {
           </p>
           {t('termAndCondition.announcement')}
           <br />
-          {Object.keys(id.termAndCondition.tnc.title).map((key, idx) => (
-            <CAccordion
-              key={key}
-              title={t(`termAndCondition.tnc.title.${idx + 1}`)}
-              description={
-                <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
-                  {t(`termAndCondition.tnc.desc.${idx + 1}`)
-                    .split('\n')
-                    .map((paragraph, index) => (
-                      <>
-                        <p key={index}>{paragraph}</p>
-                        <br />
-                      </>
-                    ))}
-                </div>
-              }
-            />
-          ))}
+
+          {/* Content Section */}
+
+          {/* Definition */}
+          <CAccordion
+            key={1}
+            title={t(`termAndCondition.tnc.title.1`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.1`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* User Statement */}
+          <CAccordion
+            key={2}
+            title={t(`termAndCondition.tnc.title.2`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.2`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Scope of service */}
+          <CAccordion
+            key={3}
+            title={t(`termAndCondition.tnc.title.3`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.3`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* User Guideline */}
+          <CAccordion
+            key={4}
+            title={t(`termAndCondition.tnc.title.4`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.4`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* USE OF PERSONAL DATA */}
+          <CAccordion
+            key={5}
+            title={t(`termAndCondition.tnc.title.5`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.5`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+
+                <ul className="list-disc pl-4 ml-6">
+                  {Object.values(
+                    t('termAndCondition.tnc.list.5', { returnObjects: true })
+                  ).map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+                <br />
+
+                {/* second paragraph */}
+                {t(`termAndCondition.tnc.desc.6`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Limitation of Liability */}
+          <CAccordion
+            key={6}
+            title={t(`termAndCondition.tnc.title.6`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.7`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Subject to user policy */}
+          <CAccordion
+            key={7}
+            title={t(`termAndCondition.tnc.title.7`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.8`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Intellectual Property */}
+          <CAccordion
+            key={7}
+            title={t(`termAndCondition.tnc.title.8`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.9`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Temporary suspension */}
+          <CAccordion
+            key={8}
+            title={t(`termAndCondition.tnc.title.9`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.10`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Service Termination */}
+          <CAccordion
+            key={9}
+            title={t(`termAndCondition.tnc.title.10`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.11`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Disclaimer */}
+          <CAccordion
+            key={10}
+            title={t(`termAndCondition.tnc.title.11`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.12`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Governing Law */}
+          <CAccordion
+            key={11}
+            title={t(`termAndCondition.tnc.title.12`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.13`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* Variance */}
+          <CAccordion
+            key={12}
+            title={t(`termAndCondition.tnc.title.13`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.14`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
+
+          {/* DISPUTE SETTLEMENT */}
+          <CAccordion
+            key={13}
+            title={t(`termAndCondition.tnc.title.14`)}
+            description={
+              <div className="flex flex-col self-stretch text-sm font-poppins text-14 leading-20 text-[#262626]">
+                {t(`termAndCondition.tnc.desc.15`)
+                  .split('\n')
+                  .map((paragraph, index) => (
+                    <>
+                      <p key={index}>{paragraph}</p>
+                      <br />
+                    </>
+                  ))}
+              </div>
+            }
+          />
         </div>
       </CardGradient>
     </PageGradient>

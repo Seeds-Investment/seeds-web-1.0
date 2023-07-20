a67bbe9d91a9f9bb2a5b7bb39db01966db59d809
Uqi <musimhujan230@gmail.com>
2023-07-20 20:16:55 +0700

    handle dialog layout

--- src/components/ui/dialog/Dialog.tsx
+++ src/components/ui/dialog/Dialog.tsx
@@ -12,13 +12,22 @@ interface Props {
   title: string
   handleClose: () => void;
   isOpen: boolean;
+  bottomSheetOnSmall?: boolean
 }
 
-const Dialog: React.FC<Props> = ({ title, handleClose, isOpen, children }) => {
+const Dialog: React.FC<Props> = ({ title, handleClose, isOpen, children, bottomSheetOnSmall=false }) => {
+  const minWidth='400px';
+  const maxWidth='420px';
+
+  const bottomSheetOnSmallClass = `max-h-[90vh] self-end mb-0 sm:self-center sm:mb-4 max-w-full sm:min-w-[${minWidth}] sm:max-w-[${maxWidth}] md:min-w-[${minWidth}] md:max-w-[${maxWidth}] lg:min-w-[${minWidth}] lg:max-w-[${maxWidth}] xl:min-w-[${minWidth}] xl:max-w-[${maxWidth}] 2xl:min-w-[${minWidth}] 2xl:max-w-[${maxWidth}]`
 
   return (
-    <TailwindDialog size="xs" open={isOpen} handler={handleClose}>
-      <DialogHeader className="justify-between">
+    <TailwindDialog
+    size="md"
+    className={bottomSheetOnSmall ? bottomSheetOnSmallClass : ''}
+    open={isOpen}
+    handler={handleClose}>
+      <DialogHeader className="justify-between w-full">
         <Typography variant="h5" color="blue-gray">
             {title}
         </Typography>

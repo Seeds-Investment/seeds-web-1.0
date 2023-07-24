a6bc02bb1839cc1df02dc059434478516e655d1d
redikawest <redikawest@gmail.com>
2023-07-24 21:51:48 +0800

    404 page

--- public/locales/id.ts
+++ public/locales/id.ts
@@ -592,5 +592,12 @@ export default {
     description:
       'Jelajahi dunia investing dan pelajari manajemen aset bersama Seeds. Download Sekarang!',
     review: '{{review}} reviews'
+  },
+  errorPage: {
+    text1: 'Oh No! Error 404',
+    text2:
+      'Sorry, your request cannot be processed at this time. Please try again later. If this problem reappears, please contact Seeds Finance customer support.',
+    button1: 'Back to Homepage',
+    button2: 'Contact our Support'
   }
 };
--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -4,42 +4,40 @@ import { Button } from '@material-tailwind/react';
 
 import { follow } from '@/repository/user.repository';
 
-
 interface Props {
-    isFollowed: boolean,
-    userId: string,
+  isFollowed: boolean;
+  userId: string;
 }
 
 export default function FollowButton({
-    isFollowed: isFollowedProp, userId
+  isFollowed: isFollowedProp,
+  userId
 }: Props): React.ReactElement {
-    const { t } = useTranslation();
-    const [isLoading, setIsLoading] = useState(false);
-    const [isFollowed, setIsFollowed] = useState(isFollowedProp);
+  const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFollowed, setIsFollowed] = useState(isFollowedProp);
 
-    const onFollow = async (): Promise<void> => {
+  const onFollow = async (): Promise<void> => {
     try {
-        setIsLoading(true);
-        const result = await follow(userId);
-        setIsFollowed(result?.status)
+      setIsLoading(true);
+      const result = await follow(userId);
+      setIsFollowed(result?.status);
     } catch (error: any) {
-        console.error('Error follow user:', error);
+      console.error('Error follow user:', error);
     } finally {
-        setIsLoading(false);
+      setIsLoading(false);
     }
   };
-    
-    return (
-        <Button
-            disabled={isLoading}
-            onClick={onFollow}
-            className={`mx-auto px-16 rounded-full normal-case ${
-            isFollowed || isLoading
-              ? 'bg-[#BDBDBD]'
-              : 'bg-[#3AC4A0]'
-          } mt-5`}
-        >
-            {isFollowed ? t('followButton.following') : t('followButton.follow')}
-        </Button>
+
+  return (
+    <Button
+      disabled={isLoading}
+      onClick={onFollow}
+      className={`mx-auto px-16 rounded-full normal-case ${
+        isFollowed || isLoading ? 'bg-[#BDBDBD]' : 'bg-[#3AC4A0]'
+      } mt-5`}
+    >
+      {isFollowed ? t('followButton.following') : t('followButton.follow')}
+    </Button>
   );
 }

97595cff654833225bf3490134b0e26f596f12d9
Redika Westama Putra <redikawest@Redikas-MacBook-Air-2.local>
2023-08-31 18:43:43 +0800

    crypto options

--- src/pages/product/components/CardAsset.tsx
+++ src/pages/product/components/CardAsset.tsx
@@ -16,6 +16,28 @@ export default function CardAsset({
     <>
       {datas !== undefined && (
         <>
+          <div className="flex flex-row items-center justify-center my-10">
+            <div className="whitespace-nowrap text-sm mr-4">
+              <span className="text-white p-3 rounded-lg text-base font-semibold bg-gradient-to-r from-[#7555DA] to-[#4FE6AF]">
+                Crypto
+              </span>
+            </div>
+            <div className="whitespace-nowrap text-sm mr-4">
+              <span className="text-white p-3 rounded-lg text-base font-semibold bg-gradient-to-r from-[#7555DA] to-[#4FE6AF]">
+                ID Stocks
+              </span>
+            </div>
+            <div className="whitespace-nowrap text-sm mr-4">
+              <span className="text-white p-3 rounded-lg text-base font-semibold bg-gradient-to-r from-[#7555DA] to-[#4FE6AF]">
+                US Stocks
+              </span>
+            </div>
+            <div className="whitespace-nowrap text-sm mr-4">
+              <span className="text-white p-3 rounded-lg text-base font-semibold bg-gradient-to-r from-[#7555DA] to-[#4FE6AF]">
+                Commodities
+              </span>
+            </div>
+          </div>
           <Card className="flex flex-row rounded-lg p-4 bg-transparent text-[#262626] mb-5 h-[60px]">
             <Typography className="w-1/3 text-base font-semibold text-start">
               Asset Name

c345e3de5120237cef3ddc021718d4a62b7a7177
Redika Westama Putra <redikawest@Redikas-MacBook-Air-2.local>
2023-08-31 11:31:34 +0800

    fix error build

--- src/pages/product/components/CardAsset.tsx
+++ src/pages/product/components/CardAsset.tsx
@@ -14,26 +14,30 @@ export default function CardAsset({
 }): React.ReactElement {
   return (
     <Card className="flex flex-row shadow-md rounded-xl mb-7 p-4 bg-transparent h-[80px] items-center">
-      <div className="flex flex-row justify-start items-center w-1/3">
-        <Avatar
-          size="md"
-          variant="circular"
-          src={data.icon}
-          className="mr-4"
-          alt="ngehe"
-        />
-        <Typography className="text-lg font-semibold text-start">
-          {data.name}
-        </Typography>
-      </div>
-      <Typography className="w-1/3 text-lg font-normal text-center">
-        {data.price}
-      </Typography>
-      <td className=" w-1/3 text-end whitespace-nowrap text-sm">
-        <span className="bg-[#27A590] text-white p-3 rounded-lg text-base font-semibold">
-          {data.change}
-        </span>
-      </td>
+      {data !== undefined && (
+        <>
+          <div className="flex flex-row justify-start items-center w-1/3">
+            <Avatar
+              size="md"
+              variant="circular"
+              src={data.icon}
+              className="mr-4"
+              alt="ngehe"
+            />
+            <Typography className="text-lg font-semibold text-start">
+              {data.name}
+            </Typography>
+          </div>
+          <Typography className="w-1/3 text-lg font-normal text-center">
+            {data.price}
+          </Typography>
+          <td className=" w-1/3 text-end whitespace-nowrap text-sm">
+            <span className="bg-[#27A590] text-white p-3 rounded-lg text-base font-semibold">
+              {data.change}
+            </span>
+          </td>
+        </>
+      )}
     </Card>
   );
 }

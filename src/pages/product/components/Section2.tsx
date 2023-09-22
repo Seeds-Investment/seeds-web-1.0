952d7dcc42f62fb1628aa1f254dd9f93989e7520
Taufik Abdul M <taufikabdul354@gmail.com>
2023-09-22 20:45:04 +0700

    adding article page

--- src/pages/product/components/Section2.tsx
+++ src/pages/product/components/Section2.tsx
@@ -8,21 +8,23 @@ import {
   Typography
 } from '@material-tailwind/react';
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import CardAsset from './CardAsset';
 import CardCircle from './CardCircle';
 
 const customGradient = (
   <>
-    <span className="z-0 fixed bottom-10 -left-10 w-60 h-48 bg-seeds-green-2 blur-[90px] rotate-45" />
-    <span className="z-0 fixed bottom-0 left-0 w-24 h-24 bg-seeds-green-2 blur-[90px]" />
-    <span className="z-0 fixed -bottom-28 left-16 w-48 h-32 bg-seeds-purple-2 blur-[90px] rotate-45" />
-    <span className="z-0 fixed top-64 -right-4 w-60 h-48 bg-seeds-green-2 blur-[90px] rotate-45 rounded-full" />
-    <span className="z-0 fixed bottom-36 right-0 w-32 h-32 bg-seeds-purple-2 blur-[90px] rotate-90 rounded-full" />
+    <span className="-z-10 fixed bottom-10 -left-10 w-60 h-48 bg-seeds-green-2 blur-[90px] rotate-45" />
+    <span className="-z-10 fixed bottom-0 left-0 w-24 h-24 bg-seeds-green-2 blur-[90px]" />
+    <span className="-z-10 fixed -bottom-28 left-16 w-48 h-32 bg-seeds-purple-2 blur-[90px] rotate-45" />
+    <span className="-z-10 fixed top-64 -right-4 w-60 h-48 bg-seeds-green-2 blur-[90px] rotate-45 rounded-full" />
+    <span className="-z-10 fixed bottom-36 right-0 w-32 h-32 bg-seeds-purple-2 blur-[90px] rotate-90 rounded-full" />
   </>
 );
 
 export default function Section2(): React.ReactElement {
   const [activeTab, setActiveTab] = useState('circle');
+  const { t } = useTranslation();
   const data = [
     {
       label: 'Top Circle',
@@ -47,11 +49,7 @@ export default function Section2(): React.ReactElement {
       className="overflow-hidden p-2 md:p-8 w-full"
     >
       <Typography className="text-3xl font-semibold mb-5 text-[#222222]">
-        Explore
-      </Typography>
-      <Typography className="text-base font-normal text-[#7C7C7C] mb-10">
-        Explore our list of communities, find the ones that match with your
-        interest, or create one.
+        {t('landingPageV2.product.section3.title1')}
       </Typography>
       <Tabs value={activeTab}>
         <TabsHeader

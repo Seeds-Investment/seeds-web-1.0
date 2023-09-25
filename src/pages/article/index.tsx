b1c80ee3d572be1c1cec818432adc59cdf5b1f31
Taufik Abdul M <taufikabdul354@gmail.com>
2023-09-25 21:53:30 +0700

    hotfix article null handler

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,5 +1,4 @@
 import handphone2 from '@/assets/landing-page/handphone2.svg';
-import Pagination from '@/components/Pagination';
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
 import Section6 from '@/containers/landing/Section6';
 import { getArticle } from '@/repository/article.repository';
@@ -68,8 +67,6 @@ export default function ArticleList(): React.ReactElement {
 
   const { t } = useTranslation();
 
-  const handlePageChange = (page: number): void => {};
-
   const timeAgo = (dateString: string): string => {
     const createdDate = new Date(dateString);
     const currentDate = new Date();
@@ -93,101 +90,99 @@ export default function ArticleList(): React.ReactElement {
   return (
     <>
       <PageGradient className="z-0 relative overflow-hidden flex flex-col justify-center mx-5 lg:mx-20">
-        <div className="flex z-10 flex-col lg:flex-row justify-between">
-          <div>
-            <div className="text-3xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
-              {t('articleList.text1')}
+        {articles !== null ? (
+          <>
+            <div className="flex z-10 flex-col lg:flex-row justify-between">
+              <div>
+                <div className="text-3xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
+                  {t('articleList.text1')}
+                </div>
+              </div>
+              <div className="lg:flex justify-end mt-4 ">
+                <div className=" hidden lg:block mt-2 font-normal text-base mx-3 text-[#7C7C7C]">
+                  {t('articleList.text3')} :
+                </div>
+                <select
+                  className="me-5 hidden lg:block text-base font-semibold"
+                  aria-label="All"
+                >
+                  <option value="option1">All</option>
+                  <option value="option2">All</option>
+                </select>
+                <input
+                  type="search"
+                  className="w-full lg:w-[300px] lg:h-[40px] rounded-3xl border-[1px] px-[8px] justify-between py-[12px] text-[#7C7C7C] "
+                  placeholder="Search"
+                  aria-label="Search"
+                  aria-describedby="button-addon2"
+                />
+              </div>
             </div>
-          </div>
-          <div className="lg:flex justify-end mt-4 ">
-            <div className=" hidden lg:block mt-2 font-normal text-base mx-3 text-[#7C7C7C]">
-              {t('articleList.text3')} :
+            <div className="lg:hidden z-10 flex justify-end mt-5">
+              <div className=" justify-end lg:hidden first-line:mt-2 font-normal text-base mx-3 text-[#7C7C7C]">
+                {t('articleList.text3')} :
+              </div>
+              <select
+                className="me-5 justify-end lg:hidden text-base font-semibold"
+                aria-label="All"
+              >
+                <option value="option1">All</option>
+                <option value="option2">All</option>
+              </select>
             </div>
-            <select
-              className="me-5 hidden lg:block text-base font-semibold"
-              aria-label="All"
-            >
-              <option value="option1">All</option>
-              <option value="option2">All</option>
-            </select>
-            <input
-              type="search"
-              className="w-full lg:w-[300px] lg:h-[40px] rounded-3xl border-[1px] px-[8px] justify-between py-[12px] text-[#7C7C7C] "
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="button-addon2"
-            />
-          </div>
-        </div>
-        <div className="lg:hidden z-10 flex justify-end mt-5">
-          <div className=" justify-end lg:hidden first-line:mt-2 font-normal text-base mx-3 text-[#7C7C7C]">
-            {t('articleList.text3')} :
-          </div>
-          <select
-            className="me-5 justify-end lg:hidden text-base font-semibold"
-            aria-label="All"
-          >
-            <option value="option1">All</option>
-            <option value="option2">All</option>
-          </select>
-        </div>
-        <div className="grid z-10 lg:grid-cols-6 gap-4 mt-8">
-          {articles.map(article => (
-            <div
-              key={article.id}
-              className="p-4 border bg-[#F9F9F9] gap-[16px] lg:col-span-2 border-gray-300 rounded w-full"
-            >
-              <Link href={`/article-list/${article.id}`}>
-                <div className="flex justify-between">
-                  <div className="flex-row">
-                    <div className="px-3 w-[30%] rounded-full font-poppins bg-[#5E44FF] text-xs font-normal">
-                      Trends
-                    </div>
-                    <div className="flex justify-between flex-col">
-                      <div className="mb-auto">
-                        <p className="mt-2 font-semibold text-base">
-                          {article.title} : {article.body}
-                        </p>
-                      </div>
+            <div className="grid z-10 lg:grid-cols-6 gap-4 mt-8">
+              {articles.map(article => (
+                <div
+                  key={article.id}
+                  className="p-4 border bg-[#F9F9F9] gap-[16px] lg:col-span-2 border-gray-300 rounded w-full"
+                >
+                  <Link href={`/article-list/${article.id}`}>
+                    <div className="flex justify-between">
+                      <div className="flex-row">
+                        <div className="px-3 w-[30%] rounded-full font-poppins bg-[#5E44FF] text-xs font-normal">
+                          Trends
+                        </div>
+                        <div className="flex justify-between flex-col">
+                          <div className="mb-auto">
+                            <p className="mt-2 font-semibold text-base">
+                              {article.title} : {article.body}
+                            </p>
+                          </div>
 
+                          <div>
+                            <div className="flex mt-2 justify-between">
+                              <p className="mt-1 font-normal text-sm text-[#8A8A8A]">
+                                {article.author}
+                              </p>
+                              <p className="mt-1 font-normal text-sm text-[#8A8A8A]">
+                                {timeAgo(article.created_at)}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                       <div>
-                        <div className="flex mt-2 justify-between">
-                          <p className="mt-1 font-normal text-sm text-[#8A8A8A]">
-                            {article.author}
-                          </p>
-                          <p className="mt-1 font-normal text-sm text-[#8A8A8A]">
-                            {timeAgo(article.created_at)}
-                          </p>
+                        <div className="mt-1 w-[100px] h-[100px]">
+                          <Image
+                            src={handphone2}
+                            alt="Image"
+                            width={100}
+                            height={100}
+                            className="h-[100px] w-[100px]"
+                          />
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div>
-                    <div className="mt-1 w-[100px] h-[100px]">
-                      <Image
-                        src={handphone2}
-                        alt="Image"
-                        width={100}
-                        height={100}
-                        className="h-[100px] w-[100px]"
-                      />
-                    </div>
-                  </div>
+                  </Link>
                 </div>
-              </Link>
+              ))}
             </div>
-          ))}
-        </div>
-
-        <div className="flex justify-center mt-8">
-          <div className="mt-5 pb-10 pagination">
-            <Pagination
-              currentPage={params.page}
-              totalPages={1}
-              onPageChange={handlePageChange}
-            />
-          </div>
-        </div>
+          </>
+        ) : (
+          <>
+            <div className="w-screen h-screen bg-white"></div>
+          </>
+        )}
       </PageGradient>
       <Section6 />
     </>

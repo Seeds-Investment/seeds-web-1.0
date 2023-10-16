4e333b9a13ac215b720836333b9932383a4bdd98
Redika Westama Putra <redikawest@Redikas-MacBook-Air-2.local>
2023-10-16 23:28:33 +0800

    feat(post pie) pop up pie

---
+++ src/components/circle/pie/CardAssetSlider.tsx
@@ -0,0 +1,66 @@
+import { ArrowTrendingUpIcon } from '@heroicons/react/24/outline';
+import {
+  Avatar,
+  Card,
+  CardBody,
+  Slider,
+  Typography
+} from '@material-tailwind/react';
+
+interface props {
+  data: any;
+  changeSlider: any;
+  index: any;
+}
+
+const CardAssetSlider: React.FC<props> = ({ data, changeSlider, index }) => {
+  return (
+    <Card shadow={false} className="w-full my-3 bg-[#F9F9F9]">
+      <CardBody className="p-3 inline-block h-auto">
+        <div className="flex flex-row items-center">
+          <Avatar
+            size="md"
+            variant="circular"
+            src={data.image}
+            alt="tania andrew"
+          />
+
+          <div className="flex ml-5 w-1/2 flex-col gap-0.5">
+            <Typography className="font-semibold text-base text-[#262626]">
+              {data.quote} / {data.currency}
+            </Typography>
+            <Typography className="font-normal text-sm text-[#7C7C7C]">
+              {data.name}
+            </Typography>
+          </div>
+
+          <div className="ml-auto flex flex-col gap-0.5">
+            <Typography className="font-semibold text-base text-[#262626]">
+              Rp {new Intl.NumberFormat().format(data.price)}
+            </Typography>
+            <Typography className="flex font-normal text-sm text-[#3AC4A0]">
+              <ArrowTrendingUpIcon height={20} width={20} className="mr-2" />
+              {data.regularPercentage.toString().substring(0, 4)}
+            </Typography>
+            <div className="bg-white inline-block rounded-full border-gray-300 border px-3 py-1">
+              <p className="font-bold text-gray-500 text-center text-sm">
+                {data.value} %
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <Slider
+          color="green"
+          className="text-[#3AC4A0] mt-5"
+          onClick={e => changeSlider(e, index)}
+          min={0}
+          max={100}
+          defaultValue={data.value}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default CardAssetSlider;
---
+++ src/containers/circle/[id]/ModalPie.tsx
@@ -0,0 +1,171 @@
+import PieAssets from '@/components/circle/pie/PieAssets';
+import PieMain from '@/components/circle/pie/PieMain';
+import { Dialog } from '@material-tailwind/react';
+import { useState } from 'react';
+
+interface ChartData {
+  labels: string[];
+  datasets: Array<{
+    data: number[];
+    backgroundColor: string[];
+  }>;
+}
+
+const initialChartData = {
+  labels: ['dummy'],
+  datasets: [
+    {
+      data: [100],
+      backgroundColor: ['#9F9F9F']
+    }
+  ]
+};
+
+interface AssetInterface {
+  id: string;
+  quote: string;
+  currency: string;
+  image: string;
+  name: string;
+  price: number;
+  regularPercentage: number;
+  value: number;
+}
+
+const ModalPie = ({ closePieModal }: any): any => {
+  const [isAsset, setIsAsset] = useState<boolean>(false);
+  const [selectedAsset, setSelectedAsset] = useState<AssetInterface[]>([]);
+  const [chartData, setChartData] = useState<ChartData>(initialChartData);
+  const [sumAsset, setSumAsset] = useState(0);
+
+  const handleOpen = (): void => {};
+
+  const handleAssets = (): void => {
+    setIsAsset(!isAsset);
+  };
+
+  const createRandomColor = (): string => {
+    const generate = Math.floor(Math.random() * 16777215).toString(16);
+
+    return '#' + generate;
+  };
+
+  const handleChartData = (value: any): void => {
+    const color = createRandomColor();
+
+    const foundDummy = chartData.labels.includes('dummy');
+    if (foundDummy) {
+      setChartData({
+        labels: [value.quote],
+        datasets: [
+          {
+            data: [0],
+            backgroundColor: [color]
+          }
+        ]
+      });
+    } else {
+      setChartData(prevState => ({
+        ...prevState,
+        labels: [...chartData.labels, value.quote],
+        datasets: [
+          {
+            ...chartData.datasets[0],
+            data: [...chartData.datasets[0].data, 0],
+            backgroundColor: [...chartData.datasets[0].backgroundColor, color]
+          }
+        ]
+      }));
+    }
+  };
+
+  const handleSelectedAsset = (e: any): void => {
+    const target = e.target;
+    const value = JSON.parse(target.value);
+
+    if (selectedAsset.length >= 8) {
+      console.error('Jumlah data maksimal telah tercapai (8)');
+      return;
+    }
+
+    const isDataExist = selectedAsset.some(item => item.id === value.id);
+
+    if (isDataExist) {
+      const newData = selectedAsset.filter(item => item.id !== value.id);
+      setSelectedAsset(newData);
+    } else {
+      handleChartData(value);
+      setSelectedAsset(oldMessages => [...oldMessages, value]);
+    }
+  };
+
+  const handleRemoveSelectedAsset = (index: number): void => {
+    const newData = [...selectedAsset];
+    newData.splice(index, 1);
+
+    setSelectedAsset(newData);
+  };
+
+  const handleSliderAsset = (e: any, index: number): void => {
+    const target = e.target;
+    const newValue = Math.round(target.value);
+
+    setChartData(prevState => {
+      const updatedData = [...prevState.datasets[0].data];
+      updatedData[index] = newValue;
+      const total = updatedData.reduce((acc, value) => acc + value, 0);
+
+      setSumAsset(total);
+      if (total <= 100) {
+        setSelectedAsset(prevState => {
+          const updatedData = [...prevState];
+          updatedData[index].value = newValue;
+
+          return updatedData;
+        });
+        return {
+          ...prevState,
+          datasets: [
+            {
+              ...prevState.datasets[0],
+              data: updatedData
+            }
+          ]
+        };
+      } else {
+        console.error('Jumlah total data tidak boleh melebihi 100');
+        return prevState;
+      }
+    });
+  };
+
+  return (
+    <Dialog
+      open={true}
+      handler={handleOpen}
+      className="max-w-full w-[90%] md:w-[50%] lg:w-[40%]"
+    >
+      <div className="bg-white rounded-lg p-14 shadow-md relative overflow-y-auto max-h-screen">
+        {isAsset ? (
+          <PieAssets
+            changeToAsset={handleAssets}
+            selectedAsset={selectedAsset}
+            handleSelectedAsset={handleSelectedAsset}
+            removeSelectedAsset={handleRemoveSelectedAsset}
+          />
+        ) : (
+          <PieMain
+            closePieModal={closePieModal}
+            chartData={chartData}
+            changeToAsset={handleAssets}
+            selectedAsset={selectedAsset}
+            changeSlider={handleSliderAsset}
+            sumAsset={sumAsset}
+          />
+        )}
+      </div>
+    </Dialog>
+  );
+};
+
+export default ModalPie;

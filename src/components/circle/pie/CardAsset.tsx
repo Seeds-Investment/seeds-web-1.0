8e8db5d1210379d802b3077fde37bfc44f948768
Redika Westama Putra <redikawest@Redikas-MacBook-Air-2.local>
2023-11-17 14:37:10 +0800

    feat(social search) fix redirect asset

--- src/components/circle/pie/CardAsset.tsx
+++ src/components/circle/pie/CardAsset.tsx
@@ -39,12 +39,14 @@ interface props {
   data: IassetsData;
   handleSelectedAsset?: any;
   isDefaultChecked?: any;
+  isClick?: boolean;
 }
 
 const CardAsset: React.FC<props> = ({
   data,
   handleSelectedAsset,
-  isDefaultChecked
+  isDefaultChecked,
+  isClick = false
 }) => {
   const router = useRouter();
   const handleArrow = (value: number): boolean => {
@@ -56,79 +58,78 @@ const CardAsset: React.FC<props> = ({
   };
 
   return (
-    <Card shadow={false} className="w-full my-3 bg-[#F9F9F9]">
-      <div
-        onClick={() => {
-          router.push(`/homepage/assets/${data.id}`).catch(error => {
-            console.log(error);
-          });
-        }}
-        className="cursor-pointer"
-      >
-        <CardBody className="p-3 inline-block h-auto">
-          <div className="flex flex-row items-center">
-            <Avatar
-              size="md"
-              variant="circular"
-              src={data.logo}
-              alt="tania andrew"
-            />
+    <Card
+      shadow={false}
+      className={`w-full my-3 bg-[#F9F9F9] ${isClick ? 'cursor-pointer' : ''}`}
+      onClick={
+        isClick
+          ? () => {
+              router.push(`/homepage/assets/${data.id}`).catch(error => {
+                console.log(error);
+              });
+            }
+          : () => {}
+      }
+    >
+      <CardBody className="p-3 inline-block h-auto">
+        <div className="flex flex-row items-center">
+          <Avatar
+            size="md"
+            variant="circular"
+            src={data.logo}
+            alt="tania andrew"
+          />
 
-            <div className="flex ml-5 w-1/2 flex-col gap-0.5">
-              <div className="flex flex-row">
-                <Typography className="font-semibold text-base text-[#262626]">
-                  {data.realTicker} /
-                </Typography>
-                <Typography className="font-normal ml-1 text-base text-[#262626]">
-                  {data.exchangeCurrency}
-                </Typography>
-              </div>
-              <Typography className="font-normal text-sm text-[#7C7C7C]">
-                {data.name}
-              </Typography>
-            </div>
-
-            <div className="ml-auto flex flex-col gap-0.5">
+          <div className="flex ml-5 w-1/2 flex-col gap-0.5">
+            <div className="flex flex-row">
               <Typography className="font-semibold text-base text-[#262626]">
-                Rp {new Intl.NumberFormat().format(data.priceBar?.open)}
+                {data.realTicker} /
               </Typography>
-              <Typography
-                className={`flex font-normal text-sm ${
-                  handleArrow(data.exchangeRate)
-                    ? 'text-[#3AC4A0]'
-                    : 'text-red-500'
-                }`}
-              >
-                {handleArrow(data.exchangeRate) ? (
-                  <ArrowTrendingUpIcon
-                    height={20}
-                    width={20}
-                    className="mr-2"
-                  />
-                ) : (
-                  <ArrowTrendingDownIcon
-                    height={20}
-                    width={20}
-                    className="mr-2"
-                  />
-                )}
-                {data.exchangeRate}
+              <Typography className="font-normal ml-1 text-base text-[#262626]">
+                {data.exchangeCurrency}
               </Typography>
             </div>
-            {handleSelectedAsset !== undefined ? (
-              <div>
-                <Checkbox
-                  className="border-[#3AC4A0]"
-                  color="green"
-                  value={JSON.stringify(data)}
-                  onClick={handleSelectedAsset}
-                  defaultChecked={isDefaultChecked(data)}
-                />
-              </div>
-            ) : null}
+            <Typography className="font-normal text-sm text-[#7C7C7C]">
+              {data.name}
+            </Typography>
           </div>
-        </CardBody>
-      </div>
+
+          <div className="ml-auto flex flex-col gap-0.5">
+            <Typography className="font-semibold text-base text-[#262626]">
+              Rp {new Intl.NumberFormat().format(data.priceBar?.open)}
+            </Typography>
+            <Typography
+              className={`flex font-normal text-sm ${
+                handleArrow(data.exchangeRate)
+                  ? 'text-[#3AC4A0]'
+                  : 'text-red-500'
+              }`}
+            >
+              {handleArrow(data.exchangeRate) ? (
+                <ArrowTrendingUpIcon height={20} width={20} className="mr-2" />
+              ) : (
+                <ArrowTrendingDownIcon
+                  height={20}
+                  width={20}
+                  className="mr-2"
+                />
+              )}
+              {data.exchangeRate}
+            </Typography>
+          </div>
+          {handleSelectedAsset !== undefined ? (
+            <div>
+              <Checkbox
+                className="border-[#3AC4A0]"
+                color="green"
+                value={JSON.stringify(data)}
+                onClick={handleSelectedAsset}
+                defaultChecked={isDefaultChecked(data)}
+              />
+            </div>
+          ) : null}
+        </div>
+      </CardBody>
     </Card>
   );
 };

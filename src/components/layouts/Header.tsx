ff77cfd70287adcea52c8598cd5f6386e0918cc8
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2023-11-30 18:17:16 +0700

    WR-220 update navbar

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,9 @@
+import BurgerMenu from '@/assets/landing-page/header/BurgerMenu.svg';
 import ChevronDown from '@/assets/landing-page/header/ChevronDown.svg';
 import SeedLogo from '@/assets/landing-page/header/SeedsLogo.svg';
-import useWindowInnerWidth from '@/hooks/useWindowInnerWidth';
-import { getUserInfo } from '@/repository/profile.repository';
 import LanguageContext from '@/store/language/language-context';
 import {
   Button,
-  IconButton,
   Menu,
   MenuHandler,
   MenuItem,
@@ -33,24 +31,9 @@ const languageList = [
   { id: 2, language: 'EN', flag: US }
 ];
 
-interface UserData {
-  name: string;
-  seedsTag: string;
-  email: string;
-  pin: string;
-  avatar: string;
-  bio: string;
-  birthDate: string;
-  phone: string;
-  _pin: string;
-}
-
 function clearLocalStorageAndRefreshPage(): void {
   // Remove specific items from local storage
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
-  localStorage.removeItem('expiresAt');
-  localStorage.removeItem('keepMeLoggedIn');
+  localStorage.clear();
 
   // Refresh the page
   window.location.reload();
@@ -59,21 +42,6 @@ function clearLocalStorageAndRefreshPage(): void {
 const Header: React.FC = () => {
   const languageCtx = useContext(LanguageContext);
   const router = useRouter();
-
-  const width = useWindowInnerWidth();
-  const [openNav, setOpenNav] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener('resize', (): void => {
-      if (window.innerWidth >= 960) {
-        setOpenNav(false);
-      }
-    });
-  }, []);
-
-  const accessToken =
-    typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
-  const [userInfo, setUserInfo] = useState<UserData | null>(null);
   const [selectedLanguage, setSelectedLanguage] = useState<'EN' | 'ID'>('EN');
 
   const handleLanguageChange = (language: 'EN' | 'ID'): void => {
@@ -81,144 +49,20 @@ const Header: React.FC = () => {
     languageCtx.languageHandler(language);
   };
 
-  useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const response = await getUserInfo();
-        setUserInfo(response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    void fetchData();
-  }, []);
-
   const logout = (): void => {
     clearLocalStorageAndRefreshPage();
   };
 
-  const navList = (
-    <ul className="flex flex-col gap-3 text-center w-full">
-      <li>
-        <Link
-          className={`${
-            router.pathname === '/' ? 'text-[#3AC4A0]' : 'text-[#7C7C7C]'
-          }`}
-          href="/"
-        >
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link
-          className={`${
-            router.pathname === '/product' ? 'text-[#3AC4A0]' : 'text-[#7C7C7C]'
-          }`}
-          href="/product"
-        >
-          Products
-        </Link>
-      </li>
-      <li>
-        <Link
-          className={`${
-            router.pathname === '/seedspedia'
-              ? 'text-[#3AC4A0]'
-              : 'text-[#7C7C7C]'
-          }`}
-          href="/seedspedia"
-        >
-          Seedspedia
-        </Link>
-      </li>
-      <li>
-        <Link
-          className={`${
-            router.pathname === '/about-us'
-              ? 'text-[#3AC4A0]'
-              : 'text-[#7C7C7C]'
-          }`}
-          href="/about-us"
-        >
-          About Us
-        </Link>
-      </li>
-      {accessToken !== null && userInfo !== null ? (
-        <></>
-      ) : (
-        <li>
-          <Link href="/auth/login" className="cursor-pointer text-white">
-            Login
-          </Link>
-        </li>
-      )}
-      <li>
-        {accessToken !== null && userInfo !== null ? (
-          <>
-            <div className="flex justify-center gap-4">
-              <div className="mt-2 font-bold text-center justify-center">
-                Hi, {userInfo.name}
-              </div>
-              <Image
-                alt="image"
-                width={17}
-                height={17}
-                className="rounded-full w-10"
-                src={userInfo.avatar}
-              />
-            </div>
-            <button
-              className="block bg-[#DD2525] mx-auto my-4 border border-transparent text-white text-base font-semibold py-2 px-8 xl:mx-8 rounded-full leading-tight focus:outline-none focus:shadow-outline"
-              onClick={logout}
-            >
-              Logout
-            </button>
-          </>
-        ) : null}
-      </li>
-      <li>
-        <div className="relative inline-flex">
-          <select
-            className="block appearance-none bg-transparent border border-transparent text-white text-base font-semibold py-2 rounded leading-tight focus:outline-none focus:shadow-outline"
-            value={selectedLanguage}
-            onChange={(e): void => {
-              handleLanguageChange(e.target.value as 'EN' | 'ID');
-            }}
-          >
-            <option value="EN">EN</option>
-            <option value="ID">ID</option>
-          </select>
-          <div className="flex items-center ml-2">
-            {languageCtx.language === 'EN' ? (
-              <Image src={US} width={16} alt="US-flag" />
-            ) : (
-              <Image src={ID} width={16} alt="ID-flag" />
-            )}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="3"
-              stroke="#FFFFFF"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </div>
-        </div>
-      </li>
-    </ul>
-  );
+  const [token, setToken] = useState();
+  useEffect(() => {
+    const storedToken: any = localStorage.getItem('accessToken');
+    setToken(storedToken);
+  }, []);
 
   return (
     <nav>
       {/* TODO: NEW HEADER */}
-      <section className="flex justify-evenly h-20 items-center">
+      <section className="xl:flex hidden justify-evenly h-20 items-center">
         <Link href="https://seeds.finance">
           <Image alt="SeedsLogo" src={SeedLogo} width={133} height={46} />
         </Link>
@@ -240,12 +84,22 @@ const Header: React.FC = () => {
           })}
         </section>
         <section className="flex items-center gap-8">
-          <Link
-            href="/auth/login"
-            className=" flex justify-center items-center cursor-pointer text-base font-semibold font-poppins text-white w-[108px] h-[42px] bg-[#3AC4A0] rounded-full"
-          >
-            Login
-          </Link>
+          {token !== null ? (
+            <div
+              onClick={logout}
+              className=" flex justify-center items-center cursor-pointer text-base font-semibold font-poppins text-white w-[108px] h-[42px] bg-[#DD2525] rounded-full"
+            >
+              Logout
+            </div>
+          ) : (
+            <Link
+              href="/auth/login"
+              className=" flex justify-center items-center cursor-pointer text-base font-semibold font-poppins text-white w-[108px] h-[42px] bg-[#3AC4A0] rounded-full"
+            >
+              Login
+            </Link>
+          )}
+
           <Menu>
             <MenuHandler>
               <Button
@@ -275,7 +129,19 @@ const Header: React.FC = () => {
                       }}
                       key={item.id}
                     >
-                      {item.language}
+                      <Button
+                        ripple={false}
+                        className="flex items-center justify-center gap-1.5 rounded-full bg-[#E9E9E9] w-[110px] h-11 hover:shadow-none shadow-none"
+                      >
+                        <Typography className="text-lg text-black font-semibold font-poppins">
+                          {item.language}
+                        </Typography>
+                        <Image
+                          src={item.flag}
+                          width={19}
+                          alt={`${item.language} flag`}
+                        />
+                      </Button>
                     </MenuItem>
                   );
                 })}
@@ -284,63 +150,110 @@ const Header: React.FC = () => {
         </section>
       </section>
       {/* TODO: END NEW HEADER */}
-      <section className="flex xl:hidden  flex-col xl:w-auto w-full">
-        <section className="flex flex-row w-full">
-          <Link href="https://seeds.finance">
+      <section className="flex xl:hidden justify-between mx-4 items-center h-20">
+        <Link href="https://seeds.finance">
+          <Image alt="SeedsLogo" src={SeedLogo} height={46} />
+        </Link>
+        <Menu
+          placement="left-start"
+          offset={-24}
+          dismiss={{ ancestorScroll: true }}
+        >
+          <MenuHandler>
             <Image
-              alt="SeedsLogo"
-              src={SeedLogo}
-              width={width !== undefined && width <= 640 ? '62.22' : undefined}
-              height={width !== undefined && width <= 640 ? '23.58' : undefined}
+              src={BurgerMenu}
+              alt="BurgerMenu"
+              className="cursor-pointer z-20"
             />
-          </Link>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={(): void => {
-              setOpenNav(!openNav);
-            }}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
-        </section>
-        {openNav ? (
-          <section className="xl:hidden flex flex-col w-full mx-auto mt-4 mb-5 text-center text-base font-semibold">
-            {navList}
-          </section>
-        ) : (
-          <></>
-        )}
+          </MenuHandler>
+          <MenuList className="pb-12 shadow-none border-none">
+            {pathUrl.map((item, index) => {
+              return (
+                <MenuItem
+                  key={item.id}
+                  className="hover:bg-transparent focus:bg-transparent"
+                >
+                  <Link
+                    href={item.url}
+                    className={` font-poppins font-normal text-base ${
+                      router.pathname === item.url
+                        ? 'text-[#3AC4A0]'
+                        : 'text-[#7C7C7C]'
+                    }`}
+                  >
+                    {item.name}
+                  </Link>
+                </MenuItem>
+              );
+            })}
+            <MenuItem className="flex justify-center hover:bg-transparent focus:bg-transparent">
+              {token !== null ? (
+                <div
+                  onClick={logout}
+                  className=" flex justify-center items-center cursor-pointer text-base font-semibold font-poppins text-white w-[108px] h-[42px] bg-[#DD2525] rounded-full"
+                >
+                  Logout
+                </div>
+              ) : (
+                <Link
+                  href="/auth/login"
+                  className=" flex justify-center items-center cursor-pointer text-base font-semibold font-poppins text-white w-[108px] h-[42px] bg-[#3AC4A0] rounded-full"
+                >
+                  Login
+                </Link>
+              )}
+            </MenuItem>
+            <MenuItem className="flex justify-center hover:bg-transparent focus:bg-transparent">
+              <Menu>
+                <MenuHandler>
+                  <Button
+                    ripple={false}
+                    className="flex items-center justify-center gap-1.5 rounded-full bg-[#E9E9E9] w-[110px] h-11 hover:shadow-none shadow-none"
+                  >
+                    <Typography className="text-lg text-black font-semibold font-poppins">
+                      {selectedLanguage}
+                    </Typography>
+                    {selectedLanguage === 'EN' ? (
+                      <Image src={US} width={30} alt="US-flag" />
+                    ) : (
+                      <Image src={ID} width={30} alt="ID-flag" />
+                    )}
+                    <Image src={ChevronDown} alt="ChevronDown" />
+                  </Button>
+                </MenuHandler>
+                <MenuList className="flex flex-col items-center p-0 bg-transparent border-none shadow-none">
+                  {languageList
+                    .filter(item => item.language !== selectedLanguage)
+                    .map((item, index) => {
+                      return (
+                        <MenuItem
+                          className="p-0 w-[110px] bg-white rounded-full"
+                          onClick={() => {
+                            handleLanguageChange(item.language as 'EN' | 'ID');
+                          }}
+                          key={item.id}
+                        >
+                          <Button
+                            ripple={false}
+                            className="flex items-center justify-center gap-1.5 rounded-full bg-[#E9E9E9] w-[110px] h-11 hover:shadow-none shadow-none"
+                          >
+                            <Typography className="text-lg text-black font-semibold font-poppins">
+                              {item.language}
+                            </Typography>
+                            <Image
+                              src={item.flag}
+                              width={19}
+                              alt={`${item.language} flag`}
+                            />
+                          </Button>
+                        </MenuItem>
+                      );
+                    })}
+                </MenuList>
+              </Menu>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </section>
     </nav>
   );

73f5ec9710619217156ce771b459f94c219f4be2
Redika Westama Putra <redikawest@Redikas-MacBook-Air-2.local>
2023-12-20 15:50:52 +0800

    add show animation in partner and about us

--- src/containers/partner/Section2.tsx
+++ src/containers/partner/Section2.tsx
@@ -5,89 +5,112 @@ import {
   SectionTwoImagePartnership
 } from '@/constants/assets/images';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import CountUp from 'react-countup';
 import { useTranslation } from 'react-i18next';
+import { useInView } from 'react-intersection-observer';
 
 const Section2: React.FC = () => {
   const { t } = useTranslation();
+  const measurement = 400;
+  const [isBottom, setBottom] = useState(0);
+  const { ref, inView, entry } = useInView({
+    threshold: 0.3
+  });
+
+  useEffect(() => {
+    const bottom = entry?.boundingClientRect.bottom ?? 0;
+    setBottom(bottom);
+  }, [entry]);
 
   return (
-    <div className="bg-white min-w-full font-poppins">
-      <div className="flex flex-col w-full items-center justify-center gap-7 font-poppins p-5 md:p-20 md:flex-row">
-        <div className="flex flex-row items-center w-full justify-start mb-5">
-          <div className="bg-[#DCFCE4] rounded-full p-2 md:p-5">
-            <Image
-              src={SectionTwoIconCap.src}
-              alt="trophy"
-              className="w-[40px] h-[40px] md:w-[66px] md:h-[66px] rounded-full"
-              width={100}
-              height={100}
-            />
+    <div className="bg-white min-w-full font-poppins" ref={ref}>
+      <div
+        className={`${
+          inView && isBottom >= measurement
+            ? 'animate-fade-in-slide'
+            : isBottom >= measurement
+            ? 'animate-fade-out-slide'
+            : ''
+        }`}
+      >
+        <div className="flex flex-col w-full items-center justify-center gap-7 font-poppins p-5 md:p-20 md:flex-row">
+          <div className="flex flex-row items-center w-full justify-start mb-5">
+            <div className="bg-[#DCFCE4] rounded-full p-2 md:p-5">
+              <Image
+                src={SectionTwoIconCap.src}
+                alt="trophy"
+                className="w-[40px] h-[40px] md:w-[66px] md:h-[66px] rounded-full"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="flex flex-col ml-4">
+              <p className="text-2xl md:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
+                <CountUp end={100} duration={3} />+
+              </p>
+              <p className="text-base md:text-xl font-normal">
+                {t('partner.section2.option1')}
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col ml-4">
-            <p className="text-2xl md:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
-              100+
-            </p>
-            <p className="text-base md:text-xl font-normal">
-              {t('partner.section2.option1')}
-            </p>
-          </div>
-        </div>
 
-        <div className="flex flex-row items-center w-full md:justify-center mb-5">
-          <div className="bg-[#DCFCE4] rounded-full p-2 md:p-5">
-            <Image
-              src={SectionTwoIconUser.src}
-              alt="trophy"
-              className="w-[40px] h-[40px] md:w-[66px] md:h-[66px] rounded-full"
-              width={100}
-              height={100}
-            />
+          <div className="flex flex-row items-center w-full md:justify-center mb-5">
+            <div className="bg-[#DCFCE4] rounded-full p-2 md:p-5">
+              <Image
+                src={SectionTwoIconUser.src}
+                alt="trophy"
+                className="w-[40px] h-[40px] md:w-[66px] md:h-[66px] rounded-full"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="flex flex-col ml-4">
+              <p className="text-2xl md:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
+                <CountUp end={100} duration={3} />+
+              </p>
+              <p className="text-base md:text-xl font-normal">
+                {t('partner.section2.option2')}
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col ml-4">
-            <p className="text-2xl md:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
-              100+
-            </p>
-            <p className="text-base md:text-xl font-normal">
-              {t('partner.section2.option2')}
-            </p>
-          </div>
-        </div>
 
-        <div className="flex flex-row items-center w-full md:justify-end mb-5">
-          <div className="bg-[#DCFCE4] rounded-full p-2 md:p-5">
-            <Image
-              src={SectionTwoIconCalendar.src}
-              alt="trophy"
-              className="w-[40px] h-[40px] md:w-[66px] md:h-[66px] rounded-full"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="flex flex-col ml-4">
-            <p className="text-2xl md:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
-              100+
-            </p>
-            <p className="text-base md:text-xl font-normal">
-              {t('partner.section2.option3')}
-            </p>
+          <div className="flex flex-row items-center w-full md:justify-end mb-5">
+            <div className="bg-[#DCFCE4] rounded-full p-2 md:p-5">
+              <Image
+                src={SectionTwoIconCalendar.src}
+                alt="trophy"
+                className="w-[40px] h-[40px] md:w-[66px] md:h-[66px] rounded-2xl"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="flex flex-col ml-4">
+              <p className="text-2xl md:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
+                <CountUp end={100} duration={3} />+
+              </p>
+              <p className="text-base md:text-xl font-normal">
+                {t('partner.section2.option3')}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col w-full items-center font-poppins mt-10">
-        <p className="text-3xl md:text-4xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF] xl:font-bold">
-          Our Partnership
-        </p>
-        <div className="relative w-full h-[450px] md:h-[600px] overflow-hidden">
-          <div className="w-full h-full object-cover">
-            <Image
-              src={SectionTwoImagePartnership.src}
-              alt="trophy"
-              width={100}
-              height={100}
-              className="absolute w-full h-full object-cover"
-            />
+        <div className="flex flex-col w-full items-center font-poppins mt-10">
+          <p className="text-3xl md:text-4xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF] xl:font-bold">
+            Our Partnership
+          </p>
+          <div className="relative w-full h-[450px] md:h-[400px] overflow-hidden">
+            <div className="w-full h-full object-cover">
+              <Image
+                src={SectionTwoImagePartnership.src}
+                alt="trophy"
+                width={100}
+                height={100}
+                className="absolute w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute bottom-0 w-full h-[25%] bg-gradient-to-t from-white to-transparent"></div>
           </div>
-          <div className="absolute bottom-0 w-full h-[25%] bg-gradient-to-t from-white to-transparent"></div>
         </div>
       </div>
     </div>

60864318410bd5cdf09a574ea00e3d0f4a10f0ec
dionisius7 <150808442+dionisius7@users.noreply.github.com>
2024-01-18 20:29:16 +0700

    fix: welcome sound enable auto play

--- src/pages/play/quiz/[id]/welcome.tsx
+++ src/pages/play/quiz/[id]/welcome.tsx
@@ -28,7 +28,7 @@ const WelcomeQuiz = () => {
     audioFiles: [
       {
         name: baseUrl + '/assets/quiz/sound/quiz_welcome_entry_signatures.mp3',
-        isAutoPlay: false
+        isAutoPlay: true
       }
     ]
   };

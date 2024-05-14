b73f3098ce66545b1f6ce0760386a40bd4a4cea0
Bimzking1 <kingofbimo@gmail.com>
2024-05-14 15:33:18 +0700

    fix: bug tournament portfolio and floating button

--- src/repository/play.repository.ts
+++ src/repository/play.repository.ts
@@ -7,6 +7,14 @@ interface ICreateOrderPlay {
   amount: number;
 }
 
+export interface AssetParams {
+  play_id: string;
+  category?: string | null;
+  currency: string;
+  per_page: number;
+  page: number;
+}
+
 const playService = baseAxios(
   `${
     process.env.NEXT_PUBLIC_URL ?? 'https://seeds-dev-gcp.seeds.finance'
@@ -367,24 +375,33 @@ export const getHistoryTransaction = async (
   }
 };
 
-export const getActiveAsset = async (
-  id: string,
-  params: { category: string; currency: string; per_page: number; page: number; }
-): Promise<any> => {
+export const getActiveAsset = async (params: AssetParams): Promise<any> => {
+  const timeoutDuration = 100000;
+
   try {
     const accessToken = localStorage.getItem('accessToken');
 
     if (accessToken === null || accessToken === '') {
       return await Promise.reject(new Error('Access token not found'));
     }
-    
-    return await playService(`/assets/active?play_id=${id}`, {
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timeoutId = setTimeout(() => { controller.abort(); }, timeoutDuration);
+
+    const response = await playService(`/assets/active`, {
       params,
       headers: {
         Accept: 'application/json',
         Authorization: `Bearer ${accessToken ?? ''}`
-      }
+      },
+      signal
     });
+
+    clearTimeout(timeoutId);
+
+    return response;
   } catch (error) {
     await Promise.reject(error);
   }

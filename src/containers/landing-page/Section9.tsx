1f02336ca29f70b5270b2c589e4e0bc38665bc68
bhanurz <aswanggab@gmail.com>
2024-05-17 13:50:28 +0700

    fix: responsive layout for section 9

--- src/containers/landing-page/Section9.tsx
+++ src/containers/landing-page/Section9.tsx
@@ -3,14 +3,18 @@ import next from '@/assets/landing-page/next.svg';
 import prev from '@/assets/landing-page/prev.svg';
 import { eventHighlightLandingPage } from '@/utils/_static/dummy';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useInView } from 'react-intersection-observer';
 import type { Settings } from 'react-slick';
 import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 import Section9Card from './Section9Card';
 
 export default function Section9(): React.ReactElement {
+  const sliderRef = useRef<Slider>(null);
+
   const { t } = useTranslation();
   const [isBottom, setBottom] = useState(0);
   const measurement = 900;
@@ -24,46 +28,60 @@ export default function Section9(): React.ReactElement {
   }, [entry]);
 
   const settings: Settings = {
-    centerMode: true,
     slidesToShow: 4,
-    speed: 3500,
+    speed: 1000,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 0,
+    autoplaySpeed: 2000,
     infinite: true,
     dots: false,
+    arrows: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          dots: false,
-          slidesToShow: 4
+          dots: true,
+          slidesToShow: 3
         }
       },
       {
         breakpoint: 768,
         settings: {
-          dots: false,
-          slidesToShow: 3
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 640,
         settings: {
-          dots: false,
-          slidesToShow: 1
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
   };
 
+  const handlePrevious = (): void => {
+    if (sliderRef.current !== null) {
+      sliderRef.current.slickPrev();
+    }
+  };
+
+  const handleNext = (): void => {
+    if (sliderRef.current !== null) {
+      sliderRef.current.slickNext();
+    }
+  };
+
   return (
     <section
       ref={ref}
-      className="h-auto min-w-full cursor-default relative font-poppins "
+      className="h-auto min-w-full cursor-default relative font-poppins py-4"
     >
       <div
-        className={`mb-10 min-w-full cursor-default font-poppins ${
+        className={`min-w-full cursor-default font-poppins ${
           inView && isBottom >= measurement
             ? 'animate-fade-in-slide'
             : isBottom >= measurement
@@ -71,37 +89,52 @@ export default function Section9(): React.ReactElement {
             : ''
         }`}
       >
-        <div className="flex flex-col xl:flex-row xl:gap-[80px] items-center justify-center mx-auto xl:px-[100px] px-6 mt-10 xl:mt-[100px]">
-          <h1 className="text-3xl lg:text-5xl font-semibold font-poppins text-center bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
-            {t('landingV2.section9.text1')}
-            <br />
+        <div className="flex flex-col justify-center items-center mt-[60px] mx-8 md:mx-[200px] xl:mx-[400px]">
+          {/* xl:flex-row xl:gap-[80px] items-center justify-center mx-auto */}
+          {/* xl:px-[100px] px-8 mt-10 xl:mt-[100px] */}
+          <h1 className="text-4xl lg:text-5xl font-semibold font-poppins text-center bg-clip-text text-transparent bg-gradient-to-r from-[#9A76FE] to-[#4FE6AF]">
+            {t('landingV2.section9.text1')} {''}
             Seeds?
           </h1>
         </div>
-        <img
+        <Image
           src="/assets/images/vector119.png"
+          width={1440}
+          height={580}
           alt=""
-          className="absolute z-0 hidden lg:block w-full h-[75%]"
+          className="absolute z-0 w-full object-cover h-[75%]"
         />
-        <div className="z-10 w-full h-full lg:mb-16 mt-10 mx-3 md:mx-20 lg:mx-6">
-          <Slider {...settings}>
+        <div className="z-10 w-full h-full lg:mb-16 mt-10">
+          <Slider ref={sliderRef} {...settings}>
             {eventHighlightLandingPage.map((data, key) => (
               <Section9Card key={key} data={data} />
             ))}
           </Slider>
         </div>
-        <div className="lg:hidden flex justify-end lg:justify-center lg:mt-2 mx-3">
+        <div className="lg:hidden flex justify-end mt-8 mx-3 py-1">
           <button
             className="rounded-full lg:p-2 border lg:mx-6 mx-3 p-1 border-1 border-[#4FE6AF]"
-            // onClick={handlePrevious}
+            onClick={handlePrevious}
           >
-            <Image src={prev} alt="Previous" className="cursor-pointer" />
+            <Image
+              src={prev}
+              width={30}
+              height={30}
+              alt="Previous"
+              className="cursor-pointer"
+            />
           </button>
           <button
             className="rounded-full lg:p-2 lg:mx-6 border mx-3 p-1 border-1 border-[#4FE6AF]"
-            // onClick={handleNext}
+            onClick={handleNext}
           >
-            <Image src={next} alt="Next" className="cursor-pointer" />
+            <Image
+              src={next}
+              width={30}
+              height={30}
+              alt="Next"
+              className="cursor-pointer"
+            />
           </button>
         </div>
       </div>

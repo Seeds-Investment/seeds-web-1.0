a53c50df87caef1d54263f929f19804c8fa83227
vitolaman <vito99varianlaman@gmail.com>
2024-05-21 13:13:48 +0700

    feat: add image and video in quiz question

--- src/utils/interfaces/quiz.interfaces.ts
+++ src/utils/interfaces/quiz.interfaces.ts
@@ -155,6 +155,8 @@ export interface QuestionDataI {
 
 export interface Answer {
   question: string;
+  question_image: string;
+  question_video: string;
   options: Options;
   description?: string;
 }

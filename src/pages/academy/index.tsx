b52a0e58dfb208e5bdcebeaf43db9705958d88c3
bhanurz <aswanggab@gmail.com>
2024-06-14 14:03:17 +0700

    feat: add seeds academy page

---
+++ src/pages/academy/index.tsx
@@ -0,0 +1,11 @@
+import PageGradient from '@/components/ui/page-gradient/PageGradient';
+
+const Academy: React.FC = () => {
+  return (
+    <PageGradient>
+      <div></div>
+    </PageGradient>
+  );
+};
+
+export default Academy;

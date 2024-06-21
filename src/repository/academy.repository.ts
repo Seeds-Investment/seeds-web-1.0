da355e1669ecc813e585c83fccbcc1e57824dc67
Mahardhika Pratama <putrad578@gmail.com>
2024-06-21 22:56:28 +0700

    fix format setup api repository seeds academy

--- src/repository/academy.repository.ts
+++ src/repository/academy.repository.ts
@@ -1,4 +1,3 @@
-import { isGuest } from '@/helpers/guest';
 import baseAxios from '@/utils/common/axios';
 import { type ClassParamsI } from '@/utils/interfaces/academy.interface';
 import { toast } from 'react-toastify';
@@ -8,27 +7,6 @@ const academyService = baseAxios(
     process.env.NEXT_PUBLIC_URL ?? 'https://seeds-dev-gcp.seeds.finance'
   }/academy/v1`
 );
-const adminService = baseAxios(
-  `${
-    process.env.NEXT_PUBLIC_URL ?? 'https://seeds-dev-gcp.seeds.finance'
-  }/admin-academy/v1`
-);
-
-export const getAllCategory = async (): Promise<any> => {
-  try {
-    const accessToken = localStorage.getItem('accessToken');
-    const response = await adminService.get(`/category`, {
-      headers: {
-        Accept: 'application/json',
-        Authorization: isGuest() ? '' : `Bearer ${accessToken ?? ''}`
-      }
-    });
-    return response.data;
-  } catch (error) {
-    toast.error('Failed to fetch fetch categories.');
-    throw error;
-  }
-};
 
 export const getAllClass = async (params: ClassParamsI): Promise<any> => {
   try {
@@ -37,13 +15,12 @@ export const getAllClass = async (params: ClassParamsI): Promise<any> => {
       params,
       headers: {
         Accept: 'application/json',
-        Authorization: isGuest() ? '' : `Bearer ${accessToken ?? ''}`
+        Authorization: `Bearer ${accessToken ?? ''}`
       }
     });
-    return response.data;
-  } catch (error) {
-    toast.error('Failed to fetch classes.');
-    throw error;
+    return response;
+  } catch (error: any) {
+    toast(error.message, { type: 'error' });
   }
 };
 
@@ -53,12 +30,11 @@ export const getClassDetail = async (id: string): Promise<any> => {
     const response = await academyService.get(`/class/${id}`, {
       headers: {
         Accept: 'application/json',
-        Authorization: isGuest() ? '' : `Bearer ${accessToken ?? ''}`
+        Authorization: `Bearer ${accessToken ?? ''}`
       }
     });
-    return response.data;
-  } catch (error) {
-    toast.error('Failed to fetch detail class.');
-    throw error;
+    return response;
+  } catch (error: any) {
+    toast(error.message, { type: 'error' });
   }
 };

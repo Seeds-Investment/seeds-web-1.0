cb738c6d950cbb81957aa1211f4aebe82d920bc7
bhanurz <aswanggab@gmail.com>
2024-06-25 12:16:07 +0700

    fix: integration api academy page

--- src/pages/academy/index.tsx
+++ src/pages/academy/index.tsx
@@ -1,12 +1,13 @@
-import Crypto from '@/assets/academy/crypto-academy.svg';
-import Finance from '@/assets/academy/finance-academy.svg';
-import Investment from '@/assets/academy/investment-academy.svg';
 import NoDataCategory from '@/assets/academy/no-data-category.svg';
-import UMKM from '@/assets/academy/umkm-academy.svg';
 import CategoryCourseCard from '@/components/academy/CategoryCourseCard';
 import LanguageSelector from '@/components/academy/LanguageSelector';
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
+import { getAllCategory } from '@/repository/academy.repository';
 import { getUserInfo } from '@/repository/profile.repository';
+import {
+  type ListCategoryAcademyI,
+  type ListParamsI
+} from '@/utils/interfaces/academy.interface';
 import { type UserInfo } from '@/utils/interfaces/tournament.interface';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { Typography } from '@material-tailwind/react';
@@ -14,10 +15,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 
 const Academy: React.FC = () => {
   const [userInfo, setUserInfo] = useState<UserInfo>();
   const { t } = useTranslation();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [categoryList, setCategoryList] = useState<ListCategoryAcademyI[]>([]);
+  const [categoryParams, setCategoryParams] = useState<ListParamsI>({
+    page: 1,
+    limit: 10,
+    search: '',
+    status: ''
+  });
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
@@ -32,44 +42,23 @@ const Academy: React.FC = () => {
       .catch(() => {});
   }, []);
 
-  const data: any[] = [
-    {
-      id: 1,
-      title: 'Investment',
-      level: 3,
-      image: Investment
-    },
-    {
-      id: 2,
-      title: 'UMKM',
-      level: 3,
-      image: UMKM
-    },
-    {
-      id: 3,
-      title: 'Finance',
-      level: 3,
-      image: Finance
-    },
-    {
-      id: 4,
-      title: 'Crypto',
-      level: 3,
-      image: Crypto
-    },
-    {
-      id: 5,
-      title: 'UMKM',
-      level: 3,
-      image: UMKM
-    },
-    {
-      id: 6,
-      title: 'Finance',
-      level: 3,
-      image: Finance
-    }
-  ];
+  useEffect(() => {
+    const fetchCategoryList = async (): Promise<void> => {
+      try {
+        setLoading(true);
+        const res = await getAllCategory(categoryParams);
+        if (res.data !== null) {
+          const data: ListCategoryAcademyI[] = res.data;
+          setCategoryList(data);
+        }
+      } catch (error) {
+        toast(`ERROR fetch list quiz ${error as string}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+    void fetchCategoryList();
+  }, [categoryParams]);
 
   return (
     <PageGradient defaultGradient className="w-full">
@@ -84,7 +73,7 @@ const Academy: React.FC = () => {
         </div>
         <div className="flex flex-col justify-start mb-3">
           <Typography className="text-white font-semibold text-xl">
-            Hello {userInfo?.name}!
+            {t('academy.greeting')} {userInfo?.name}!
           </Typography>
           <Typography className="text-white text-base">
             {t('academy.courseTitle')}
@@ -96,10 +85,13 @@ const Academy: React.FC = () => {
             <input
               id="search"
               type="text"
-              // value={search}
-              // onChange={e => {
-              //   handleSearch(e);
-              // }}
+              value={categoryParams.search}
+              onChange={e => {
+                setCategoryParams(prev => ({
+                  ...prev,
+                  search: e.target.value
+                }));
+              }}
               name="search"
               placeholder={t('academy.courseSearchPlaceholder') ?? ''}
               className="block w-full text-[#262626] text-sm h-10 leading-4 placeholder:text-[#BDBDBD] focus:outline-0 disabled:bg-[#E9E9E9] p-2 pl-10 rounded-xl border border-[#BDBDBD]"
@@ -113,15 +105,7 @@ const Academy: React.FC = () => {
             {t('academy.categoryTitle')}
           </Typography>
         </div>
-        {data.length > 0 ? (
-          <div className="grid lg:grid-cols-3 grid-cols-2 gap-6 lg:gap-x-8 gap-y-4">
-            {data.map((item, index) => (
-              <Link key={index} href={`/academy/${item.id as number}`}>
-                <CategoryCourseCard data={item} />
-              </Link>
-            ))}
-          </div>
-        ) : (
+        {categoryList.length === 0 && !loading && (
           <div className="flex flex-col justify-center items-center">
             <Image
               src={NoDataCategory}
@@ -135,6 +119,19 @@ const Academy: React.FC = () => {
             <Typography className="text-sm text-[#7C7C7C]">
               {t('academy.noCategoriesDescription')}
             </Typography>
+          </div>
+        )}
+        {loading ? (
+          <div className="col-span-3 flex items-center justify-center">
+            <div className="animate-spinner w-5 h-5" />
+          </div>
+        ) : (
+          <div className="grid lg:grid-cols-3 grid-cols-2 gap-6 lg:gap-x-8 gap-y-4">
+            {categoryList.map((item, index) => (
+              <Link key={index} href={`/academy/${item.id}`}>
+                <CategoryCourseCard data={item} />
+              </Link>
+            ))}
           </div>
         )}
       </div>

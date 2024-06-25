685d1c1bb2dff948a529290a31f85c8610e5c47d
bhanurz <aswanggab@gmail.com>
2024-06-25 12:17:07 +0700

    fix: category card

--- src/components/academy/CategoryCourseCard.tsx
+++ src/components/academy/CategoryCourseCard.tsx
@@ -1,27 +1,24 @@
+import { type ListCategoryAcademyI } from '@/utils/interfaces/academy.interface';
 import { Typography } from '@material-tailwind/react';
 import Image from 'next/image';
 import React from 'react';
 
-interface CategoryCourseCardI {
-  data: {
-    title: string;
-    level: string;
-    image: string;
-  };
-}
-
-const CategoryCourseCard: React.FC<CategoryCourseCardI> = ({ data }) => {
+const CategoryCourseCard: React.FC<{ data: ListCategoryAcademyI }> = ({
+  data
+}) => {
   return (
     <div className="flex flex-col justify-center items-center bg-[#DCFCE4] rounded-2xl py-6">
       <Typography className="font-semibold text-sm lg:text-lg">
         {data.title}
       </Typography>
       <Typography className="text-xs lg:text-base text-[#7C7C7C]">
-        {data.level} levels
+        {data.level.length} levels
       </Typography>
       <Image
-        src={data.image}
+        src={data?.banner}
         alt={data.title}
+        width={125}
+        height={125}
         className="w-[100px] h-[100px] lg:w-[125px] lg:h-[125px]"
       />
     </div>

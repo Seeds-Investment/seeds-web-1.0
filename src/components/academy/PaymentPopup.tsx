5bad395e8e916c624b4deae7a66bd1f67fcf970a
Mahardhika Pratama <putrad578@gmail.com>
2024-07-01 18:36:44 +0700

    fix handle responsive use tailwind

--- src/components/academy/PaymentPopup.tsx
+++ src/components/academy/PaymentPopup.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { IoCloseSharp } from 'react-icons/io5';
 
@@ -20,20 +20,7 @@ const PaymentPopup: React.FC<PaymentPopupProps> = ({
 }) => {
   const router = useRouter();
   const { id } = router.query;
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
   const { t } = useTranslation();
-
-  const checkScreenSize = (): void => {
-    setIsSmallScreen(window.innerWidth < 768);
-  };
-
-  useEffect(() => {
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
-    return () => {
-      window.removeEventListener('resize', checkScreenSize);
-    };
-  }, []);
 
   const handleSubmit = (): void => {
     void router.push(
@@ -44,132 +31,65 @@ const PaymentPopup: React.FC<PaymentPopupProps> = ({
 
   if (!isOpen) return null;
 
-  if (isSmallScreen) {
-    return (
-      <>
-        <div
-          className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50 z-40"
-          onClick={onClose}
-        />
-        <div className="fixed bottom-0 left-0 right-0 z-50 flex items-end justify-center">
-          <div className="bg-white rounded-t-2xl overflow-hidden w-full border-2">
-            <div className="flex justify-center my-5">
-              <button
-                onClick={onClose}
-                className="bg-[#E9E9E9] py-1 px-14 text-xl rounded-3xl"
-              ></button>
+  return (
+    <>
+      <div
+        className="fixed inset-0 bg-black opacity-50 z-40"
+        onClick={onClose}
+      />
+      <div className="fixed inset-0 flex items-end justify-center md:items-center z-50">
+        <div className="bg-white rounded-t-2xl md:rounded-2xl overflow-hidden w-full md:w-3/4 lg:w-1/2 border-2">
+          <div className="px-4 pt-4 flex justify-between items-center">
+            <div className="text-lg font-bold">
+              {t('academy.payment.enroll')}
             </div>
-            <div className="px-2">
-              <div className="text-lg font-bold">
-                {t('academy.payment.enroll')}
-              </div>
-              <div className="text-base text-[#7C7C7C]">{classTitle}</div>
-            </div>
-            <div className="px-2 py-2 flex flex-col gap-3">
-              <div className="flex justify-center">
-                <div className="w-1/2 h-24 rounded-lg bg-[#FDBA22] px-2 pt-7 pb-2">
-                  <div className="bg-white w-full h-full flex flex-col justify-center items-center">
-                    <div className="text-center font-bold text-[#FDBA22]">
-                      {amount}
-                    </div>
-                    <div className="text-center text-xs text-[#FDBA22]">
-                      {t('academy.payment.class')}
-                    </div>
+            <button onClick={onClose} className="text-2xl md:mt-2">
+              <IoCloseSharp />
+            </button>
+          </div>
+          <div className="px-4 text-base text-[#7C7C7C]">{classTitle}</div>
+          <div className="px-4 py-4 flex flex-col gap-3">
+            <div className="flex justify-center">
+              <div className="w-1/2 h-24 rounded-lg bg-[#FDBA22] px-2 pt-7 pb-2">
+                <div className="bg-white w-full h-full flex flex-col justify-center items-center">
+                  <div className="text-center font-bold text-[#FDBA22]">
+                    {amount}
+                  </div>
+                  <div className="text-center text-xs text-[#FDBA22]">
+                    {t('academy.payment.class')}
                   </div>
                 </div>
               </div>
-              <div className="text-center text-[#7C7C7C]">
-                {t('academy.payment.desc')}
-              </div>
-              {/* Feature below not developed yet */}
-              <div className="flex flex-col gap-1 text-[#D89918] cursor-not-allowed opacity-30">
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 75.000 /Month
-                </div>
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 150.000 /3 Month
-                </div>
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 250.000 /6 Month
-                </div>
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 350.000 /12 Month
-                </div>
-              </div>
-              <button
-                onClick={handleSubmit}
-                className="w-full bg-[#3AC4A0] text-white py-2 px-4 rounded-3xl"
-              >
-                {t('academy.payment.buttonPay')}
-              </button>
             </div>
+            <div className="text-center text-[#7C7C7C]">
+              {t('academy.payment.desc')}
+            </div>
+            {/* Feature below not developed yet */}
+            <div className="flex flex-col gap-1 text-[#D89918] cursor-not-allowed opacity-30">
+              <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
+                IDR 75.000 /Month
+              </div>
+              <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
+                IDR 150.000 /3 Month
+              </div>
+              <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
+                IDR 250.000 /6 Month
+              </div>
+              <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
+                IDR 350.000 /12 Month
+              </div>
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="w-full bg-[#3AC4A0] text-white py-2 px-4 rounded-3xl"
+            >
+              {t('academy.payment.buttonPay')}
+            </button>
           </div>
         </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div
-          className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50 z-40"
-          onClick={onClose}
-        />
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-3/4 lg:w-1/2">
-          <div className="bg-white rounded-2xl overflow-hidden w-full border-2">
-            <div className="px-4 pt-4">
-              <div className="flex flex-row items-center justify-between">
-                <div className="text-lg font-bold">
-                  {t('academy.payment.enroll')}
-                </div>
-                <button onClick={onClose} className="text-2xl">
-                  <IoCloseSharp />
-                </button>
-              </div>
-              <div className="text-base text-[#7C7C7C]">{classTitle}</div>
-            </div>
-            <div className="px-4 py-4 flex flex-col gap-3">
-              <div className="flex justify-center">
-                <div className="w-1/2 h-24 rounded-lg bg-[#FDBA22] px-2 pt-7 pb-2">
-                  <div className="bg-white w-full h-full flex flex-col justify-center items-center">
-                    <div className="text-center font-bold text-[#FDBA22]">
-                      {amount}
-                    </div>
-                    <div className="text-center text-xs text-[#FDBA22]">
-                      {t('academy.payment.class')}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="text-center text-[#7C7C7C]">
-                {t('academy.payment.desc')}
-              </div>
-              {/* Feature below not developed yet */}
-              <div className="flex flex-col gap-1 text-[#D89918] cursor-not-allowed opacity-30">
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 75.000 /Month
-                </div>
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 150.000 /3 Month
-                </div>
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 250.000 /6 Month
-                </div>
-                <div className="bg-[#FFF7D2] py-3 w-full text-center rounded-lg">
-                  IDR 350.000 /12 Month
-                </div>
-              </div>
-              <button
-                onClick={handleSubmit}
-                className="w-full bg-[#3AC4A0] text-white py-2 px-4 rounded-3xl"
-              >
-                {t('academy.payment.buttonPay')}
-              </button>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 };
 
 export default PaymentPopup;
--- src/components/popup/ModalShareCourse.tsx
+++ src/components/popup/ModalShareCourse.tsx
@@ -2,7 +2,6 @@ import CopyLink from '@/assets/play/tournament/copyTournamentLink.svg';
 import { Typography } from '@material-tailwind/react';
 import Image from 'next/image';
 import { XIcon } from 'public/assets/vector';
-import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
@@ -16,119 +15,58 @@ const ModalShareCourse: React.FC<Props> = ({ onClose, url }) => {
   const baseUrl =
     process.env.NEXT_PUBLIC_DOMAIN ?? 'https://user-dev-gcp.seeds.finance';
 
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-  const checkScreenSize = (): void => {
-    setIsSmallScreen(window.innerWidth < 768);
-  };
-  useEffect(() => {
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
-    return () => {
-      window.removeEventListener('resize', checkScreenSize);
-    };
-  }, []);
-
   const handleCopyClick = async (): Promise<void> => {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     const textToCopy = `${baseUrl}/academy/course/${url}`;
     await navigator.clipboard.writeText(textToCopy).then(() => {
       toast('Course link copied!');
     });
   };
 
-  if (isSmallScreen) {
-    return (
-      <>
-        <div
-          className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50 z-40"
-          onClick={onClose}
-        />
-        <div className="fixed bottom-0 left-0 right-0 z-50 flex items-end justify-center">
-          <div className="bg-white rounded-t-3xl overflow-hidden w-full border p-6">
-            <div className="flex justify-between mb-4">
-              <Typography className="font-semibold text-lg">
-                {t('academy.shareCourse')}
-              </Typography>
-              <Image
-                src={XIcon}
-                alt="X"
-                width={30}
-                height={30}
-                onClick={onClose}
-                className="hover:scale-110 transition ease-out cursor-pointer"
+  return (
+    <>
+      <div
+        className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50 z-40"
+        onClick={onClose}
+      />
+      <div className="fixed inset-0 flex items-end justify-center sm:items-center z-50">
+        <div className="bg-white rounded-t-2xl sm:rounded-2xl overflow-hidden w-full sm:w-3/4 lg:w-1/2 border-2 p-3">
+          <div className="flex justify-between mb-4">
+            <Typography className="font-semibold text-lg">
+              {t('academy.shareCourse')}
+            </Typography>
+            <Image
+              src={XIcon}
+              alt="X"
+              width={30}
+              height={30}
+              onClick={onClose}
+              className="hover:scale-110 transition ease-out cursor-pointer"
+            />
+          </div>
+          <div className="flex flex-col gap-2 justify-center px-2 lg:px-8 pt-2 items-center text-center">
+            <Typography className="text-base">
+              {t('academy.shareLinks')}
+            </Typography>
+            <div className="w-full h-fit flex mb-4">
+              <input
+                type="text"
+                readOnly={true}
+                disabled={false}
+                value={`${baseUrl}/academy/course/${url}`}
+                className="block w-full text-[#262626] h-10 leading-4 placeholder-text-[#BDBDBD] focus:outline-none disabled:bg-[#E9E9E9] p-4 rounded-xl border border-[#BDBDBD]"
               />
-            </div>
-            <div className="flex flex-col gap-2 justify-center px-2 lg:px-8 pt-2 items-center text-center">
-              <Typography className="text-base">
-                {t('academy.shareLinks')}
-              </Typography>
-              <div className="w-full h-fit flex mb-4">
-                <input
-                  type="text"
-                  readOnly={true}
-                  disabled={false}
-                  value={`${baseUrl}/academy/course/${url}`}
-                  className="block w-full text-[#262626] h-10 leading-4 placeholder:text-[#BDBDBD] focus:outline-0 disabled:bg-[#E9E9E9] p-4 rounded-xl border border-[#BDBDBD]"
-                />
-                <div
-                  onClick={handleCopyClick}
-                  className="w-[50px] cursor-pointer flex justify-center items-center"
-                >
-                  <Image alt="copy-link" src={CopyLink} className="w-[20px]" />
-                </div>
+              <div
+                onClick={handleCopyClick}
+                className="w-12 lg:w-[50px] cursor-pointer flex justify-center items-center"
+              >
+                <Image alt="copy-link" src={CopyLink} className="w-[20px]" />
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div
-          className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50 z-40"
-          onClick={onClose}
-        />
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-3/4 lg:w-1/2">
-          <div className="bg-white rounded-3xl overflow-hidden w-full border p-6">
-            <div className="flex justify-between mb-4">
-              <Typography className="font-semibold text-lg">
-                {t('academy.shareCourse')}
-              </Typography>
-              <Image
-                src={XIcon}
-                alt="X"
-                width={30}
-                height={30}
-                onClick={onClose}
-                className="hover:scale-110 transition ease-out cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-2 justify-center px-2 lg:px-8 pt-2 items-center text-center">
-              <Typography className="text-base">
-                {t('academy.shareLinks')}
-              </Typography>
-              <div className="w-full h-fit flex mb-4">
-                <input
-                  type="text"
-                  readOnly={true}
-                  disabled={false}
-                  value={`${baseUrl}/academy/course/${url}`}
-                  className="block w-full text-[#262626] h-10 leading-4 placeholder:text-[#BDBDBD] focus:outline-0 disabled:bg-[#E9E9E9] p-4 rounded-xl border border-[#BDBDBD]"
-                />
-                <div
-                  onClick={handleCopyClick}
-                  className="w-[50px] cursor-pointer flex justify-center items-center"
-                >
-                  <Image alt="copy-link" src={CopyLink} className="w-[20px]" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 };
 
 export default ModalShareCourse;

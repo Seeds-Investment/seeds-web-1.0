1ad16f57b3c13cdfac1c44489af1ae7692c36080
bhanurz <aswanggab@gmail.com>
2024-07-03 14:33:02 +0700

    fix: adjust the button order

--- src/containers/homepage/FeatureSection.tsx
+++ src/containers/homepage/FeatureSection.tsx
@@ -12,21 +12,6 @@ const FeatureSection: React.FC = (): React.ReactElement => {
   return (
     <div className="flex justify-between gap-2 lg:px-4 py-2 xl:px-8 xl:gap-4">
       <div
-        onClick={async () => await router.push('/play')}
-        className="w-full flex flex-col lg:gap-2 justify-center items-center border border-[#E9E9E9] rounded-lg px-2 py-2 lg:p-4 hover:border-[#3AC4A0] hover:shadow-lg duration-300 cursor-pointer"
-      >
-        <Image
-          width={100}
-          height={100}
-          alt=""
-          src={PlayArena}
-          className="w-[40px] h-[40px] lg:w-[35px] lg:h-[35px]"
-        />
-        <div className="flex justify-center items-center text-center text-[#262626] text-xs lg:text-[16px] mt-1 lg:mt-0">
-          Play
-        </div>
-      </div>
-      <div
         onClick={async () => await router.push('/academy')}
         className="w-full flex flex-col lg:gap-2 justify-center items-center border border-[#E9E9E9] rounded-lg px-2 py-2 lg:p-4 hover:border-[#3AC4A0] hover:shadow-lg duration-300 cursor-pointer"
       >
@@ -39,6 +24,21 @@ const FeatureSection: React.FC = (): React.ReactElement => {
         />
         <div className="flex justify-center items-center text-center text-[#262626] text-[9px] lg:text-[16px] mt-1 lg:mt-0">
           Seeds Academy
+        </div>
+      </div>
+      <div
+        onClick={async () => await router.push('/play')}
+        className="w-full flex flex-col lg:gap-2 justify-center items-center border border-[#E9E9E9] rounded-lg px-2 py-2 lg:p-4 hover:border-[#3AC4A0] hover:shadow-lg duration-300 cursor-pointer"
+      >
+        <Image
+          width={100}
+          height={100}
+          alt=""
+          src={PlayArena}
+          className="w-[40px] h-[40px] lg:w-[35px] lg:h-[35px]"
+        />
+        <div className="flex justify-center items-center text-center text-[#262626] text-xs lg:text-[16px] mt-1 lg:mt-0">
+          Play
         </div>
       </div>
       <div

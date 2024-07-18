8396a775dbf6a469a6a85c7f943a6cf184f1c6d6
Mahardhika Pratama <putrad578@gmail.com>
2024-07-18 21:46:16 +0700

    fix & update wording quiz detail

--- src/pages/play/quiz/[id]/index.tsx
+++ src/pages/play/quiz/[id]/index.tsx
@@ -215,14 +215,14 @@ const QuizDetail = (): React.ReactElement => {
             </div>
           </div>
           <div className="mt-4">
-            <div className="text-lg font-semibold">Quiz Period</div>
+            <div className="text-lg font-semibold">{t('quiz.quizPeriod')}</div>
             <div className="text-lg text-[#7C7C7C]">
               {moment(detailQuiz?.started_at).format('D MMM YYYY, h a')} Jakarta
               - {moment(detailQuiz?.ended_at).format('D MMM YYYY, h a')} Jakarta
             </div>
           </div>
           <div className="mt-4">
-            <div className="text-lg font-semibold">Terms & Conditions</div>
+            <div className="text-lg font-semibold">{t('quiz.tnc')}</div>
 
             <div
               className="text-lg text-[#7C7C7C]"
@@ -236,7 +236,7 @@ const QuizDetail = (): React.ReactElement => {
             />
           </div>
           <div className="mt-4">
-            <div className="text-lg font-semibold">Quiz Prize</div>
+            <div className="text-lg font-semibold">{t('quiz.quizPrize')}</div>
             <table className="mt-2">
               {detailQuiz?.prizes?.map((item, i) => (
                 <tr key={i}>
@@ -277,7 +277,9 @@ const QuizDetail = (): React.ReactElement => {
           <div className="mt-4 flex flex-row gap-8">
             {detailQuiz?.sponsors?.image_url ? (
               <div className="flex flex-col justify-center items-center gap-4">
-                <div className="text-lg font-semibold">{'Sponsor(s)'}</div>
+                <div className="text-lg font-semibold">
+                  {t('quiz.sponsors')}
+                </div>
                 <Image
                   src={detailQuiz?.sponsors?.image_url}
                   alt=""
@@ -289,7 +291,9 @@ const QuizDetail = (): React.ReactElement => {
             ) : null}
             {detailQuiz?.communities?.image_url ? (
               <div className="flex flex-col justify-center items-center gap-4">
-                <div className="text-lg font-semibold">{'Community'}</div>
+                <div className="text-lg font-semibold">
+                  {t('quiz.community')}
+                </div>
                 <Image
                   src={detailQuiz?.communities?.image_url}
                   alt=""
@@ -322,7 +326,7 @@ const QuizDetail = (): React.ReactElement => {
                 onChange={e => {
                   setInvitationCode(e.target.value);
                 }}
-                placeholder="Invitation Code"
+                placeholder={`${t('quiz.invitationCodePlaceholder')}`}
                 className="w-full border p-2 rounded-md mt-2"
               />
             </div>
@@ -343,7 +347,7 @@ const QuizDetail = (): React.ReactElement => {
               <Image src={goldSeedsCoin} alt="Next" width={30} height={30} />
               <div className="text-xs text-[#7C7C7C]">
                 {totalAvailableCoins > 0
-                  ? `Redeem ${totalAvailableCoins} seeds coin`
+                  ? t('quiz.seedsCoin', { data: totalAvailableCoins })
                   : `Coin cannot be redeemed`}
               </div>
             </div>

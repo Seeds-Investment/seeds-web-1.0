8ec0780c2f80a02a8ab7804f6f37ac3ae6a7eee5
bhanurz <aswanggab@gmail.com>
2024-08-30 16:20:08 +0700

    feat: add integration transaction history

--- src/pages/seedsplan/history.tsx
+++ src/pages/seedsplan/history.tsx
@@ -1,21 +1,31 @@
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
 import { getUserInfo } from '@/repository/profile.repository';
+import { getTransactionHistory } from '@/repository/subscription.repository';
 import { type UserInfo } from '@/utils/interfaces/earning.interfaces';
+import { type TransactionHistoryRes } from '@/utils/interfaces/subscription.interface';
+import { Typography } from '@material-tailwind/react';
 import moment from 'moment';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import GoldPlan from 'public/assets/subscription/gold-plan.svg';
 import SilverPlan from 'public/assets/subscription/silver-plan.svg';
+import { ArrowBackwardIcon } from 'public/assets/vector';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
 const HistoryTransaction: React.FC = () => {
   const { t } = useTranslation();
+  const router = useRouter();
   const [userInfo, setUserInfo] = useState<UserInfo>();
+  const [dataTransaction, setDataTransaction] =
+    useState<TransactionHistoryRes>();
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
+        const transactionHistory = await getTransactionHistory();
+        setDataTransaction(transactionHistory);
         const dataInfo = await getUserInfo();
         setUserInfo(dataInfo);
       } catch (error) {
@@ -27,73 +37,24 @@ const HistoryTransaction: React.FC = () => {
       .catch(() => {});
   }, []);
 
-  const data = [
-    {
-      id: 1,
-      name_plan: 'Gold',
-      transaction_date: '2022-01-01',
-      status: 'success',
-      amount: 30000
-    },
-    {
-      id: 2,
-      name_plan: 'Silver',
-      transaction_date: '2022-02-15',
-      status: 'canceled',
-      amount: 20000
-    },
-    {
-      id: 3,
-      name_plan: 'Gold',
-      transaction_date: '2022-03-10',
-      status: 'success',
-      amount: 50000
-    },
-    {
-      id: 4,
-      name_plan: 'Silver',
-      transaction_date: '2022-04-25',
-      status: 'canceled',
-      amount: 15000
-    },
-    {
-      id: 5,
-      name_plan: 'Gold',
-      transaction_date: '2022-05-30',
-      status: 'success',
-      amount: 25000
-    },
-    {
-      id: 6,
-      name_plan: 'Gold',
-      transaction_date: '2022-05-30',
-      status: 'success',
-      amount: 25000
-    },
-    {
-      id: 7,
-      name_plan: 'Gold',
-      transaction_date: '2022-05-30',
-      status: 'success',
-      amount: 25000
-    },
-    {
-      id: 8,
-      name_plan: 'Silver',
-      transaction_date: '2022-04-25',
-      status: 'canceled',
-      amount: 15000
-    }
-  ];
-
   return (
     <PageGradient defaultGradient className="w-full">
       <div className="bg-white p-5 rounded-[18px] shadow-lg">
-        <h1 className="font-semibold text-xl">
-          {t('subscription.transactionHistory')}
-        </h1>
-        <div>
-          {data.map(item => (
+        <div className="flex items-center justify-center relative">
+          <Image
+            src={ArrowBackwardIcon}
+            alt="arrow-backward-icon"
+            onClick={() => {
+              router.back();
+            }}
+            className="cursor-pointer absolute left-0"
+          />
+          <Typography className="font-semibold text-xl">
+            {t('subscription.transactionHistory')}
+          </Typography>
+        </div>
+        {dataTransaction?.data !== null ? (
+          dataTransaction?.data?.map(item => (
             <div
               key={item.id}
               className="flex justify-between items-center p-3 my-2 bg-[#F9F9F9] rounded-xl hover:shadow-md duration-300"
@@ -102,14 +63,18 @@ const HistoryTransaction: React.FC = () => {
                 <div>
                   <div
                     className={`absolute w-[50px] h-[50px] rounded-full bg-gradient-to-b ${
-                      item.name_plan === 'Gold'
+                      item?.subscription_type === 'GOLD'
                         ? 'from-[#b5dda4] to-[#FEEBA6]'
                         : 'from-[#a8dbce] to-[#d7e5e1]'
                     }`}
                   ></div>
                   <div className="relative left-2 top-1">
                     <Image
-                      src={item.name_plan === 'Gold' ? GoldPlan : SilverPlan}
+                      src={
+                        item?.subscription_type === 'GOLD'
+                          ? GoldPlan
+                          : SilverPlan
+                      }
                       alt="Plan Subscription"
                       width={35}
                       height={35}
@@ -117,32 +82,40 @@ const HistoryTransaction: React.FC = () => {
                   </div>
                 </div>
                 <div className="flex-col">
-                  <p className="font-semibold text-sm">{item.name_plan} Plan</p>
+                  <p className="font-semibold text-sm capitalize">
+                    {item?.subscription_type.toLocaleLowerCase()} Plan
+                  </p>
                   <p className="text-xs text-[#BDBDBD]">
-                    {moment(item.transaction_date).format('dddd, DD MMM YYYY')}
+                    {moment(item?.created_at).format('dddd, DD MMM YYYY')}
                   </p>
                 </div>
               </div>
               <div className="flex-col">
                 <p className="text-xs text-[#BDBDBD]">
-                  {item.amount.toLocaleString('id-ID', {
+                  {item?.paid_price.toLocaleString('id-ID', {
                     currency: userInfo?.preferredCurrency ?? 'IDR',
                     style: 'currency'
                   })}
                 </p>
                 <p
                   className={`capitalize text-sm font-semibold ${
-                    item.status === 'success'
+                    item?.payment_status?.toLowerCase() === 'success'
                       ? 'text-green-500'
                       : 'text-red-500'
                   }`}
                 >
-                  {item.status}
+                  {item?.payment_status?.toLocaleLowerCase()}
                 </p>
               </div>
             </div>
-          ))}
-        </div>
+          ))
+        ) : (
+          <div className="p-3 my-2 bg-[#F9F9F9] rounded-xl">
+            <Typography className="text-[#a5a5a5] font-poppins">
+              Transaction history is empty
+            </Typography>
+          </div>
+        )}
       </div>
     </PageGradient>
   );

4a344464196e4406679706829d0028f38e9b4cbf
Mahardhika Pratama <putrad578@gmail.com>
2024-10-10 16:17:24 +0700

    style: font title arena

--- src/pages/play/team-battle/[id]/arena.tsx
+++ src/pages/play/team-battle/[id]/arena.tsx
@@ -280,7 +280,7 @@ const BattleHome: React.FC = () => {
           >
             <IoArrowBack size={30} />
           </div>
-          <div className="text-center text-lg sm:text-xl lg:text-2xl col-span-1 font-poppins">
+          <div className="text-center font-semibold text-lg sm:text-xl lg:text-2xl col-span-1 font-poppins">
             {t('teamBattle.battleCompetition')}
           </div>
           <div className="col-span-1" />

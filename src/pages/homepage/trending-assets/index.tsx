bb43fc564fa88d625400b3dcab42fdaeb4c381a0
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2024-11-04 16:48:04 +0700

    fix: sorting system

--- src/pages/homepage/trending-assets/index.tsx
+++ src/pages/homepage/trending-assets/index.tsx
@@ -34,7 +34,7 @@ const initialFilter: Filter = {
   search: '',
   limit: 10,
   page: 1,
-  sortBy: 'rating',
+  sortBy: 'most_traded',
   currency: ''
 };
 
@@ -46,10 +46,9 @@ const initialMetadata: Metadata = {
 };
 
 const optionSortBy = [
-  { label: 'All', value: '' },
-  { label: 'Top Gainers', value: 'top gainers' },
-  { label: 'Top Lowest', value: 'top lowest' },
-  { label: 'Most Trades', value: 'most trades' }
+  { label: 'Most Traded', value: 'most_traded' },
+  { label: 'Top Gainers', value: 'top_gainers' },
+  { label: 'Top Losers', value: 'top_losers' }
 ];
 
 export default function ListAssets(): React.ReactElement {
@@ -79,10 +78,7 @@ export default function ListAssets(): React.ReactElement {
   const fetchDataAssets = async (): Promise<void> => {
     try {
       setIsLoading(true);
-      const response = await getTrendingAssets({
-        ...filter,
-        sortBy: 'most_traded'
-      });
+      const response = await getTrendingAssets(filter);
       if (response.data === null) {
         setAssets([]);
       } else {
@@ -201,7 +197,7 @@ export default function ListAssets(): React.ReactElement {
             onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
               setFilter(prevState => ({
                 ...prevState,
-                sort_by: e.target.value
+                sortBy: e.target.value
               }));
             }}
           >

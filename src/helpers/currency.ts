f443b549ee6cd69a601ad7c1c70d211b77cc0c66
hansss12 <raihan.aqil2003@gmail.com>
2024-11-25 11:07:28 +0700

    fix bug

--- src/helpers/currency.ts
+++ src/helpers/currency.ts
@@ -42,7 +42,7 @@ export const calculatePercentageDifference = (
     };
   } else if (percentageDifference < 0) {
     result = {
-      value: Math.abs(percentageDifference).toFixed(2),
+      value: percentageDifference.toFixed(2),
       isUpTrend: false,
       isDownTrend: true
     };

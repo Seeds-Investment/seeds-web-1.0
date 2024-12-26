5be292f837ca5e7414160a7c0dce708acb7045f5
bhanurz <aswanggab@gmail.com>
2024-12-26 15:01:46 +0700

    feat: add interface for danamart

---
+++ src/utils/interfaces/danamart.interface.ts
@@ -0,0 +1,11 @@
+export interface RegistrationForm {
+  namaDepan: string;
+  namaBlkng: string;
+  email: string;
+  tipePermodal: string;
+  nohp: string;
+  password: string;
+  repassword: string;
+  kode_referral: string;
+  notifikasi: string;
+}

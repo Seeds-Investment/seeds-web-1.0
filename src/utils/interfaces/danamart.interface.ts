54075cc8fbf0a10cacb7773dd6b3a74651ab3b42
bhanurz <aswanggab@gmail.com>
2025-01-03 16:18:20 +0700

    feat: add interface for user dashboard

--- src/utils/interfaces/danamart.interface.ts
+++ src/utils/interfaces/danamart.interface.ts
@@ -9,3 +9,57 @@ export interface RegistrationForm {
   kode_referral: string;
   notifikasi: string;
 }
+
+export interface UserDashboard {
+  StatusCode: string;
+  Status: string;
+  message: string;
+  dataSaldoUser: DataSaldoUser;
+  dataChart: DataChart;
+  mobileApps: MobileApps;
+}
+
+export interface DataSaldoUser {
+  danaCash: string;
+  dana_cash: number;
+  total_wd0: number;
+  total_dana_dalam_efek: number;
+  danaRewerd: string;
+  TotalDanaRewerd: number;
+  danaInterim: string;
+  TotalDanaTersedia: string;
+  DanaDapatDiinvestasikan: string;
+  TotalDanadalamEfek: string;
+  totalDana: string;
+  danaBisaTarik: string;
+  batasPembelian: number;
+  sisaPembelian: string;
+  pendapatPertahun: number;
+}
+
+export interface DataChart {
+  totalPembelianSektor: number;
+  jmlSektor: number;
+  chartjs: Chartjs;
+  apexchart: Apexchart;
+}
+
+export interface Chartjs {
+  persenEbus: string;
+  persenEbe: string;
+}
+
+export interface Apexchart {
+  SektorUsaha: string;
+  jmlSektor: string;
+}
+
+export interface MobileApps {
+  banner: Banner;
+}
+
+export interface Banner {
+  image: string;
+  type: string;
+  url: string;
+}

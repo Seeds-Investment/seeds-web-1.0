d3ea6859c57c35b12bf9e77a3e9a508a4be48b3d
bhanurz <aswanggab@gmail.com>
2025-01-06 12:01:20 +0700

    feat: add fetch user profile data and verify user data

--- src/pages/danamart/dashboard.tsx
+++ src/pages/danamart/dashboard.tsx
@@ -4,10 +4,16 @@ import SecuritiesChart from '@/components/danamart/SecuritiesChart';
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
 import { decryptResponse } from '@/helpers/cryptoDecrypt';
 import withAuthDanamart from '@/helpers/withAuthDanamart';
-import { getDashboardUser } from '@/repository/danamart/danamart.repository';
-import { type UserDashboard } from '@/utils/interfaces/danamart.interface';
+import {
+  getDashboardUser,
+  getProfileUser
+} from '@/repository/danamart/danamart.repository';
+import {
+  type UserDashboard,
+  type UserProfile
+} from '@/utils/interfaces/danamart.interface';
 import { Typography } from '@material-tailwind/react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   PiChartLineUp,
@@ -23,30 +29,71 @@ const Dashboard = (): React.ReactElement => {
   const { t } = useTranslation();
 
   const [dashboardData, setDashboardData] = useState<UserDashboard>();
+  const [userProfileData, setUserProfileData] = useState<UserProfile>();
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchDashboardUser = async (): Promise<void> => {
     try {
-      setIsLoading(true);
-      const response = await getDashboardUser();
-      if (response?.status === 200) {
-        const encryptedData = response?.data;
-        const decryptedData = decryptResponse(encryptedData);
-        if (decryptedData !== null) {
-          const parseDecryptedData = JSON.parse(decryptedData);
-          setDashboardData(parseDecryptedData);
-        }
+      const dashboard = await getDashboardUser();
+      if (dashboard?.status === 200) {
+        const decryptedData = JSON.parse(
+          decryptResponse(dashboard.data) !== null
+            ? decryptResponse(dashboard.data)
+            : dashboard.data
+        );
+        setDashboardData(decryptedData);
       }
     } catch (error) {
-      toast.error(t('danamart.dashboard.errorGetUserDashboard'));
-    } finally {
-      setIsLoading(false);
+      toast.error(t('danamart.dashboard.errorGetDashboard'));
+    }
+  };
+
+  const fetchUserProfile = async (): Promise<void> => {
+    try {
+      const profile = await getProfileUser();
+      if (profile?.status === 200) {
+        const decryptedProfile = JSON.parse(
+          decryptResponse(profile.data) !== null
+            ? decryptResponse(profile.data)
+            : profile.data
+        );
+        setUserProfileData(decryptedProfile);
+      }
+    } catch (error) {
+      toast.error(t('danamart.dashboard.errorGetUserProfile'));
     }
   };
 
   useEffect(() => {
-    void fetchDashboardUser();
+    setIsLoading(true);
+    Promise.all([fetchDashboardUser(), fetchUserProfile()]).finally(() => {
+      setIsLoading(false);
+    });
   }, []);
+
+  const renderVerifyData = useCallback(() => {
+    if (userProfileData === undefined) return null;
+
+    const status = userProfileData?.detailUser[0];
+    const message =
+      status?.status_email === '0' || status?.status_hp === '0'
+        ? t('danamart.dashboard.verifyPhone')
+        : status?.status_form === '0'
+        ? t('danamart.dashboard.verifyForm')
+        : '';
+
+    return message !== '' ? (
+      <div className="w-full bg-[#DA2D1F33] px-4 py-3">
+        <Typography className="text-[#DA2D1F] font-poppins font-normal text-sm">
+          {message} {''}
+          <span className="text-[#36b996] underline cursor-pointer">
+            {t('danamart.dashboard.here')}
+          </span>
+        </Typography>
+      </div>
+    ) : null;
+  }, [userProfileData, t]);
 
   const dashboardCardData: Array<{
     id: number;
@@ -55,6 +102,7 @@ const Dashboard = (): React.ReactElement => {
     icon: React.ReactNode;
     title: string;
     desc: string;
+    value: string;
   }> = [
     {
       id: 1,
@@ -67,40 +115,47 @@ const Dashboard = (): React.ReactElement => {
         />
       ),
       title: t('danamart.dashboard.cashInHand'),
-      desc: t('danamart.dashboard.cashInHandDesc')
+      desc: t('danamart.dashboard.cashInHandDesc'),
+      value: dashboardData?.dataSaldoUser?.danaCash ?? 'Rp. 0'
     },
     {
       id: 2,
       icon: <PiGift size={26} color="#9A76FE" />,
       title: t('danamart.dashboard.danaReward'),
-      desc: t('danamart.dashboard.danaRewardDesc')
+      desc: t('danamart.dashboard.danaRewardDesc'),
+      value: dashboardData?.dataSaldoUser?.danaRewerd ?? 'Rp. 0'
     },
     {
       id: 3,
       icon: <PiDatabaseLight size={26} color="#9A76FE" />,
       title: t('danamart.dashboard.danaInterim'),
-      desc: t('danamart.dashboard.danaInterimDesc')
+      desc: t('danamart.dashboard.danaInterimDesc'),
+      value: dashboardData?.dataSaldoUser?.danaInterim ?? 'Rp. 0'
     },
     {
       id: 4,
       reverseBg: true,
       icon: <PiChartLineUp size={26} color="#9A76FE" />,
       title: t('danamart.dashboard.investableFunds'),
-      desc: t('danamart.dashboard.investableFundsDesc')
+      desc: t('danamart.dashboard.investableFundsDesc'),
+      value: dashboardData?.dataSaldoUser?.TotalDanaTersedia ?? 'Rp. 0'
     },
     {
       id: 5,
       icon: <PiCoinsLight size={26} color="#9A76FE" />,
       title: t('danamart.dashboard.fundsSecurities'),
-      desc: t('danamart.dashboard.fundsSecuritiesDesc')
+      desc: t('danamart.dashboard.fundsSecuritiesDesc'),
+      value: dashboardData?.dataSaldoUser?.TotalDanadalamEfek ?? 'Rp. 0'
     },
     {
       id: 6,
       icon: <PiWalletLight size={26} color="#9A76FE" />,
       title: t('danamart.dashboard.totalFunds'),
-      desc: t('danamart.dashboard.totalFundsDesc')
+      desc: t('danamart.dashboard.totalFundsDesc'),
+      value: dashboardData?.dataSaldoUser?.totalDana ?? 'Rp. 0'
     }
   ];
+
   return (
     <PageGradient defaultGradient className="w-full">
       <div className="flex flex-col md:gap-8 gap-5 bg-white md:py-8 md:px-5 py-5 px-4">
@@ -114,17 +169,10 @@ const Dashboard = (): React.ReactElement => {
                 {t('danamart.dashboard.description')}
               </Typography>
             </div>
-            <div className="w-full bg-[#DA2D1F33] px-4 py-3">
-              <Typography className="text-[#DA2D1F] font-poppins font-normal text-sm">
-                {t('danamart.dashboard.verifyPhone')} {''}
-                <span className="text-[#36b996] underline cursor-pointer">
-                  {t('danamart.dashboard.here')}
-                </span>
-              </Typography>
-            </div>
+            {renderVerifyData()}
             <div className="flex flex-wrap flex-grow items-center justify-center gap-4 w-full place-items-center">
-              {dashboardCardData.map((item, index) => (
-                <DashboardCard key={index} data={item} />
+              {dashboardCardData.map(item => (
+                <DashboardCard key={item?.id} data={item} />
               ))}
             </div>
             <div className="flex flex-grow flex-wrap items-center justify-center gap-8 w-full py-2">

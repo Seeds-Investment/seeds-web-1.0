1110ebde2eb6c68fa254712e458fb06c978e77c8
bhanurz <aswanggab@gmail.com>
2025-01-06 12:00:11 +0700

    feat: add type for user profile response

--- src/utils/interfaces/danamart.interface.ts
+++ src/utils/interfaces/danamart.interface.ts
@@ -63,3 +63,69 @@ export interface Banner {
   type: string;
   url: string;
 }
+
+export interface UserProfile {
+  StatusCode: string;
+  Status: string;
+  message: string;
+  Pengguna: string;
+  User: User;
+  detailUser: DetailUser[];
+  namaPerusahaan: string;
+  detailDigisign: any;
+  detailDokumen: any[];
+  dataRekening: DataRekening;
+  tarikdana: string;
+  danakeluar: any;
+}
+
+export interface User {
+  id: string;
+  login_id: string;
+  nama: string;
+  email: string;
+  email_sosmed: any;
+  nohp: string;
+  access: string;
+  status: string;
+}
+
+export interface DetailUser {
+  user_pendana_id: string;
+  username_digisign: string;
+  paymentcode_bca: string;
+  paymentcode_bri: string;
+  paymentcode_mandiri: string;
+  paymentcode_bni: string;
+  customer_id: string;
+  fva_id: string;
+  ext_fva_id: string;
+  rdl_id: string;
+  rdl_ext_id: string;
+  rdl_cif_number: string;
+  rdl_account_number: string;
+  sre: any;
+  sid: any;
+  panggilan: string;
+  nama: string;
+  email: string;
+  slug_url: string;
+  handphone: string;
+  referral_id_lv1: string;
+  referral_id_lv2: string;
+  notifikasi: string;
+  status_email: string;
+  status_hp: string;
+  status_form: string;
+  status_digisign: string;
+  doc_digisign: any;
+  create_date: string;
+}
+
+export interface DataRekening {
+  RekBni: string;
+  RekBri: string;
+  RekMandiri: string;
+  RekPermata: string;
+  RekBca: string;
+}

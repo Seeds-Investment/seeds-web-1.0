a37b8d701da2b24e7f3d8bc455846f195fa61a12
bhanurz <aswanggab@gmail.com>
2025-01-13 14:21:54 +0700

    feat: adjust verify page

--- src/pages/danamart/dashboard/verify.tsx
+++ src/pages/danamart/dashboard/verify.tsx
@@ -1,18 +1,59 @@
+import FinancialInformation from '@/components/danamart/verify/FinancialInformation';
 import PhotoIdCard from '@/components/danamart/verify/photo-id-card';
 import PageGradient from '@/components/ui/page-gradient/PageGradient';
 import withAuthDanamart from '@/helpers/withAuthDanamart';
+import { Typography } from '@material-tailwind/react';
+import Image from 'next/image';
+import { ArrowTaillessLeft } from 'public/assets/vector';
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Verify = (): React.ReactElement => {
-	const [step, setStep] = useState<string>('id-card');
-	
+  const { t } = useTranslation();
+  const [step, setStep] = useState<number>(3);
+
+  const titles: Record<number, string> = {
+    1: t('danamart.verification.photoIdCardTittle'),
+    2: t('danamart.verification.accountInformationTittle'),
+    3: t('danamart.verification.financialInformationTittle'),
+    4: t('danamart.verification.photoSelfieTittle')
+  };
+
+  const getBarClass = (currentStep: number): string =>
+    currentStep <= step ? 'bg-seeds-button-green' : 'bg-[#E9E9E9]';
+
   return (
     <PageGradient defaultGradient className="w-full">
-      <div className="flex flex-col md:gap-8 gap-5 bg-white md:py-8 md:px-5 py-5 px-4 rounded-2xl">
-				{
-					step === 'id-card' &&
-						<PhotoIdCard step={step} setStep={setStep}/>
-				}
+      <div className="flex flex-col md:gap-8 gap-5 bg-white md:py-6 md:px-5 py-5 px-4 rounded-2xl">
+        <div className="w-full flex justify-start items-center gap-4">
+          <Image
+            src={ArrowTaillessLeft}
+            alt="ArrowTaillessLeft"
+            width={16}
+            height={16}
+            className="cursor-pointer"
+            onClick={() => {
+              if (step > 1) setStep(step - 1);
+            }}
+          />
+          <Typography className="font-poppins font-semibold text-md">
+            {titles[step]}
+          </Typography>
+          <Typography className="font-poppins font-normal text-sm text-[#BDBDBD]">
+            {step} of 4
+          </Typography>
+        </div>
+        <div className="w-full flex gap-4">
+          {[1, 2, 3, 4].map(currentStep => (
+            <div
+              key={currentStep}
+              className={`h-[6px] w-full ${getBarClass(currentStep)}`}
+            />
+          ))}
+        </div>
+        {step === 1 && <PhotoIdCard step={step} setStep={setStep} />}
+        {step === 2 && <div>2</div>}
+        {step === 3 && <FinancialInformation />}
       </div>
     </PageGradient>
   );

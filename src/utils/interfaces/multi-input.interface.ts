3029f7f5feeff3adeedf035dcb1823c6a2d7f492
bhanurz <aswanggab@gmail.com>
2025-01-20 11:44:09 +0700

    feat: add interface for input toggle

--- src/utils/interfaces/multi-input.interface.ts
+++ src/utils/interfaces/multi-input.interface.ts
@@ -1,11 +1,11 @@
 import {
-    type Control,
-    type FieldErrors,
-    type FieldValues,
-    type Path,
-    type UseFormRegister,
-    type UseFormWatch,
-} from "react-hook-form";
+  type Control,
+  type FieldErrors,
+  type FieldValues,
+  type Path,
+  type UseFormRegister,
+  type UseFormWatch
+} from 'react-hook-form';
 
 export interface Option {
   key: number;
@@ -15,7 +15,7 @@ export interface Option {
 
 interface Data {
   label: string;
-  value: any
+  value: any;
 }
 
 interface CommonProps<T extends FieldValues> {
@@ -26,7 +26,7 @@ interface CommonProps<T extends FieldValues> {
 }
 
 interface CommonIProps<T extends FieldValues> extends CommonProps<T> {
-  type: "text" | "datetime-local" | "email";
+  type: 'text' | 'datetime-local' | 'email';
   register: UseFormRegister<T>;
   disabled?: boolean;
   maxLength?: number;
@@ -35,7 +35,7 @@ interface CommonIProps<T extends FieldValues> extends CommonProps<T> {
 }
 
 interface NumberIProps<T extends FieldValues> extends CommonProps<T> {
-  type: "number";
+  type: 'number';
   watch: UseFormWatch<T>;
   control: Control<T, any>;
   locale?: string;
@@ -44,11 +44,12 @@ interface NumberIProps<T extends FieldValues> extends CommonProps<T> {
   prefix?: string;
   suffix?: string;
   decimalsLimit?: number;
-  max?:number
+  max?: number;
+  extraClasses?: string;
 }
 
 interface CheckboxIProps<T extends FieldValues> extends CommonProps<T> {
-  type: "checkbox";
+  type: 'checkbox';
   labelCheckbox: string;
   value: any;
   register: UseFormRegister<T>;
@@ -56,14 +57,14 @@ interface CheckboxIProps<T extends FieldValues> extends CommonProps<T> {
 }
 
 interface RadioIProps<T extends FieldValues> extends CommonProps<T> {
-  type: "radio";
+  type: 'radio';
   data: Data[];
   register: UseFormRegister<T>;
   disabled?: boolean;
 }
 
 interface ImageIProps<T extends FieldValues> extends CommonProps<T> {
-  type: "image";
+  type: 'image';
   imageURLPreview: string | undefined;
   dataImage?: string;
   isCrop?: boolean;
@@ -72,7 +73,7 @@ interface ImageIProps<T extends FieldValues> extends CommonProps<T> {
 }
 
 interface DropdownProps<T extends FieldValues> extends CommonProps<T> {
-  type: "dropdown";
+  type: 'dropdown';
   registerName: Path<T>;
   control?: Control<T, any>;
   options?: Option[];
@@ -81,10 +82,18 @@ interface DropdownProps<T extends FieldValues> extends CommonProps<T> {
   fullWidth?: boolean;
 }
 
+interface SwitchToggleProps<T extends FieldValues> extends CommonProps<T> {
+  control?: Control<T, any>;
+  registerName: Path<T>;
+  type: 'switch';
+  disabled?: boolean;
+}
+
 export type MultiProps<T extends FieldValues> =
   | CommonIProps<T>
   | NumberIProps<T>
   | CheckboxIProps<T>
   | RadioIProps<T>
   | ImageIProps<T>
-  | DropdownProps<T>;
+  | DropdownProps<T>
+  | SwitchToggleProps<T>;

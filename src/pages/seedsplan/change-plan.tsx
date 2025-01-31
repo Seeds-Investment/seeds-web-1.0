321d510eedafd2f78d243b7d867b0b76bf7cf632
HusinHanbasy <husinhanbasy88@gmail.com>
2025-01-31 13:56:28 +0700

    fix active subription

--- src/pages/seedsplan/change-plan.tsx
+++ src/pages/seedsplan/change-plan.tsx
@@ -39,13 +39,12 @@ const ChangePlan: React.FC = () => {
   const [userInfo, setUserInfo] = useState<UserInfo>();
 
   const [dataPlan, setDataPlan] = useState<DataPlanI>();
+
   const [mappedPlansByTier, setMappedPlansByTier] = useState<PlanByTier[]>([]);
   const [allAvailablePlans, setAllAvailablePlans] = useState<PlanI[]>([]);
   const [selectedPeriodPlan, setSelectedPeriodPlan] = useState<PlanI>();
-
   const [packagePlan, setPackagePlan] = useState<string>('SILVER');
   const [periodPlan, setPeriodPlan] = useState<number>(1);
-
   const [isActiveSubscription, setIsActiveSubscription] =
     useState<boolean>(false);
   const [showTnc, setShowTnc] = useState<boolean>(false);
@@ -113,7 +112,6 @@ const ChangePlan: React.FC = () => {
         { name: 'GOLD', data: [...GOLD] },
         { name: 'PLATINUM', data: [...PLATINUM] }
       ];
-
       setMappedPlansByTier(mappedPlan);
       setAllAvailablePlans([...SILVER, ...GOLD, ...PLATINUM]);
       setPeriodPlan(
@@ -128,19 +126,10 @@ const ChangePlan: React.FC = () => {
   )?.data;
 
   useEffect(() => {
-    if (isActiveSubscription && subscriptionStatus !== null) {
-      const activeSubscriptionId =
-        subscriptionStatus?.active_subscription?.subscription_type_id;
-      const selected = allAvailablePlans?.find(
-        item => item.id === activeSubscriptionId
-      );
-      setSelectedPeriodPlan(selected);
-    } else {
-      const selected = filteredPlanByTier?.find(
-        item => item.duration_in_months === periodPlan
-      );
-      setSelectedPeriodPlan(selected);
-    }
+    const selected = filteredPlanByTier?.find(
+      item => item.name === packagePlan
+    );
+    setSelectedPeriodPlan(selected);
   }, [
     periodPlan,
     filteredPlanByTier,
@@ -174,6 +163,14 @@ const ChangePlan: React.FC = () => {
       ''
     );
   };
+  const getPeriodPlan = (): number => {
+    const activeSubscriptionId =
+      subscriptionStatus?.active_subscription?.subscription_type_id;
+    return (
+      allAvailablePlans.find(item => item.id === activeSubscriptionId)
+        ?.duration_in_months ?? 1
+    );
+  };
 
   return (
     <>
@@ -198,7 +195,7 @@ const ChangePlan: React.FC = () => {
                     strokeWidth={0.75}
                   />
                   <div className="text-xl md:text-2xl font-bold text-center mx-auto">
-                    MySeeds Plan
+                    My Seeds Plan
                   </div>
                 </div>
                 <div className="md:mt-4 flex flex-row gap-2 items-center mb-4 w-full">
@@ -389,7 +386,10 @@ const ChangePlan: React.FC = () => {
                       </div>
                       <button
                         onClick={async () => {
-                          if (getActivePlan() === packagePlan) {
+                          if (
+                            getActivePlan() === packagePlan &&
+                            periodPlan === getPeriodPlan()
+                          ) {
                             await router.push('/play');
                           } else {
                             await router.push(
@@ -399,7 +399,8 @@ const ChangePlan: React.FC = () => {
                         }}
                         className="w-full py-3 bg-[#3ac4a0] rounded-3xl font-semibold transform scale-100 hover:scale-105 transition-transform duration-300 my-2"
                       >
-                        {getActivePlan() === packagePlan
+                        {getActivePlan() === packagePlan &&
+                        periodPlan === getPeriodPlan()
                           ? t('seedsPlan.button8')
                           : t('seedsPlan.button6')}
                       </button>

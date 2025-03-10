cbca5ba749671d47ba4f699e9afa9f8e6d6ff6b0
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2025-03-10 11:05:13 +0700

    feat:edit env

--- src/repository/gass.repository.ts
+++ src/repository/gass.repository.ts
@@ -11,7 +11,8 @@ export const gassPost = async (payload: {
     await axios.post(`https://z26.gass.co.id/api.html`, {
       ...payload,
       project_key:
-        process.env.GASS_PROJECT_KEY ?? '939E98001B6C92B322B0F42C05121F1E'
+        process.env.NEXT_PUBLIC_GASS_PROJECT_KEY ??
+        '939E98001B6C92B322B0F42C05121F1E'
     });
   } catch (error) {}
 };

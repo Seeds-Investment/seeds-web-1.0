41a2b959e94aa2fe9f09d1cb56c54dbc6874b00b
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2025-03-19 13:00:54 +0700

    feat:change cta and logo

--- src/pages/ads/index.tsx
+++ src/pages/ads/index.tsx
@@ -5,13 +5,7 @@ import React from 'react';
 
 const Ads = (): React.ReactElement => {
   const { type } = useRouter().query;
-  return type === 'shuffle' ? (
-    <QuizShuffle />
-  ) : type === 'wa' ? (
-    <QuizPlay cta="https://gass.seeds.finance/cta?p=939E98001B6C92B322B0F42C05121F1E&divisi=qontak&msg=ID+%5B_gid_%5D%25break%25Hai+Min+Seeds%2C%25break%25Saya+Mau+Daftar+Event+Web3%25break%25Apakah+Promonya+masih+ada%3F" />
-  ) : (
-    <QuizPlay />
-  );
+  return type === 'shuffle' ? <QuizShuffle /> : <QuizPlay />;
 };
 
 export default Ads;

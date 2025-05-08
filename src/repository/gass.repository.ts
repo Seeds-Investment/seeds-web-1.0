28a59fb34bc02e42dae28fb52e921d9cc8924e07
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2025-05-08 12:53:12 +0700

    fix gass code

--- src/repository/gass.repository.ts
+++ src/repository/gass.repository.ts
@@ -8,11 +8,25 @@ export const gassPost = async (payload: {
   event?: 'prospek' | 'purchase';
 }): Promise<void> => {
   try {
-    await axios.post(`https://z26.gass.co.id/api.html`, {
-      ...payload,
-      project_key:
-        process.env.NEXT_PUBLIC_GASS_PROJECT_KEY ??
+    const data = new URLSearchParams();
+    data.append('act', payload.act);
+    if (payload.visitor_id !== null)
+      data.append('visitor_id', payload.visitor_id ?? '');
+    if (payload.phone !== null) data.append('phone', payload.phone ?? '');
+    if (payload.email !== null) data.append('email', payload.email ?? '');
+    if (payload.event !== null) data.append('event', payload.event ?? '');
+    data.append(
+      'project_key',
+      process.env.NEXT_PUBLIC_GASS_PROJECT_KEY ??
         '939E98001B6C92B322B0F42C05121F1E'
+    );
+
+    await axios.post('https://z26.gass.co.id/api.html', data, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     });
-  } catch (error) {}
+  } catch (error) {
+    console.error(error);
+  }
 };
